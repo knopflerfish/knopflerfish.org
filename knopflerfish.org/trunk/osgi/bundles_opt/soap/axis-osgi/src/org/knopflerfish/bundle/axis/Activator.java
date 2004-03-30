02f56b4a0f92635d97560631fd3e0a0e0057fdbb
helander <helander@87908148-3fd6-0310-a573-b04dead93e22>
2004-03-30 10:57:20 +0000

    Tidy things up etc.

--- knopflerfish.org/trunk/osgi/bundles_opt/soap/axis-osgi/src/org/knopflerfish/bundle/axis/Activator.java
+++ knopflerfish.org/trunk/osgi/bundles_opt/soap/axis-osgi/src/org/knopflerfish/bundle/axis/Activator.java
@@ -1,56 +1,124 @@
+/*
+ * Copyright (c) 2003-2004, KNOPFLERFISH project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above/*
+ * Copyright (c) 2003-2004, KNOPFLERFISH project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials
+ *   provided with the distribution.
+ *
+ * - Neither the name of the KNOPFLERFISH project nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.knopflerfish.bundle.axis;
 
+import java.io.InputStream;
 
-import org.apache.axis.server.AxisServer;
+import java.net.URL;
+
 import org.apache.axis.EngineConfiguration;
 import org.apache.axis.configuration.FileProvider;
+import org.apache.axis.server.AxisServer;
 
+import org.knopflerfish.service.log.LogRef;
+
+import org.knopflerfish.util.servlet.ServletDescriptor;
+import org.knopflerfish.util.servlet.WebApp;
+import org.knopflerfish.util.servlet.WebAppDescriptor;
 
 import org.osgi.framework.*;
 
-import java.io.InputStream;
-import java.net.URL;
 
+/** The <code>Activator</code> is the activator for the Axis OSGi bundle.
+ *
+ * @author Lasse Helander (lars-erik.helander@home.se)
+ */
+public class Activator
+   implements BundleActivator {
+   public static BundleContext axisBundle = null;
+   public static LogRef log = null;
+   private static AxisServer axisServer = null;
+   private WebApp webApp = null;
 
-public class Activator extends org.knopflerfish.util.servlet.WebApp {
-  public static BundleContext axisBundle = null;
-  private static AxisServer axisServer = null;
+   public static AxisServer getAxisServer() {
+      return axisServer;
+   }
 
-  
-  public static AxisServer getAxisServer() {return axisServer;}
- 
-  public WebAppDescriptor getWebAppDescriptor() {
-    WebAppDescriptor webApp = new WebAppDescriptor();
-    webApp.servlet = new ServletDescriptor[1];    
-    webApp.context = "/axis";
-    webApp.servlet[0] = new ServletDescriptor("/services",new ServicesServlet());
+   public void start(BundleContext bc)
+              throws BundleException {
+      try {
+         log = new LogRef(bc, true);
+         axisBundle = bc;
+         URL url = this.getClass().getResource("/axis/server-config.wsdd");
+         InputStream is = url.openStream();
+         EngineConfiguration fromBundleResource = new FileProvider(is);
 
-    return webApp;
-  }
-  
-  public void start(BundleContext bc) throws BundleException {
-    try {
-      axisBundle = bc;
-      URL url = this.getClass().getResource("/axis/server-config.wsdd");
-      InputStream is = url.openStream();
-      EngineConfiguration fromBundleResource = new FileProvider(is);
-      axisServer = new AxisServer(fromBundleResource);
-      super.start(bc);
-    } catch (Exception e) {
-      throw new BundleException("Failed to start server");
-    }
-  }
-  
-  public void stop(BundleContext bc) throws BundleException {
-    try {
-        super.stop(bc);
-        axisBundle = null;
-        axisServer = null;
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new BundleException("Failed to stop server", e);
-    }
-  }
-  
+         log.info("Configuration file read.");
+         axisServer = new AxisServer(fromBundleResource);
+         log.info("Axis server started.");
+         webApp = new WebApp(getWebAppDescriptor());
+         webApp.start(bc);
+         log.info("Web application started.");
+      } catch (Exception e) {
+         log.error("Exception when starting bundle", e);
+         throw new BundleException("Failed to start server");
+      }
+   }
+
+   public void stop(BundleContext bc)
+             throws BundleException {
+      try {
+         webApp.stop(bc);
+         webApp = null;
+         axisBundle = null;
+         axisServer = null;
+         log.close();
+         log = null;
+      } catch (Exception e) {
+         log.error("Exception when stopping bundle", e);
+         throw new BundleException("Failed to stop server", e);
+      }
+   }
+
+   private WebAppDescriptor getWebAppDescriptor() {
+      WebAppDescriptor wad = new WebAppDescriptor();
+
+      wad.servlet = new ServletDescriptor[1];
+      wad.context = "/axis";
+      wad.servlet[0] = new ServletDescriptor("/services", 
+                                             new ServicesServlet());
+      return wad;
+   }
 }
-
