5dd84627d3ed7653d8dd2b285041a98475e4e5eb
wistrand <wistrand@87908148-3fd6-0310-a573-b04dead93e22>
2004-04-06 08:57:53 +0000

    ported r132 from trunk

--- knopflerfish.org/branches/wip/osgi/framework/src/org/knopflerfish/framework/FilterImpl.java
+++ knopflerfish.org/branches/wip/osgi/framework/src/org/knopflerfish/framework/FilterImpl.java
@@ -35,6 +35,7 @@
 package org.knopflerfish.framework;
 
 import java.util.Dictionary;
+import java.util.Hashtable;
 
 import org.osgi.framework.*;
 
@@ -50,7 +51,19 @@ public class FilterImpl implements Filter {
 
 
   public boolean match(ServiceReference reference) {
-    return ldap.evaluate(((ServiceReferenceImpl)reference).getProperties());
+    if(reference instanceof ServiceReferenceImpl) {
+      // This is the normal case
+      return ldap.evaluate(((ServiceReferenceImpl)reference).getProperties());
+    } else {
+      // This might happen if we live in a remote framework world
+      // Copy the properties the hard way
+      Hashtable props = new Hashtable();
+      String[] keys = reference.getPropertyKeys();
+      for(int i = 0; i < keys.length; i++) {
+	props.put(keys[i], reference.getProperty(keys[i]));
+      }
+      return ldap.evaluate(props);
+    }
   }
 
 
