1d3a38b558f5c12894cd5ccd0df90c1e44adbaaf
wistrand <wistrand@87908148-3fd6-0310-a573-b04dead93e22>
2004-04-07 10:06:04 +0000

    better docs

--- osgi/bundles/remotefw/src/org/knopflerfish/service/remotefw/RemoteFramework.java
+++ osgi/bundles/remotefw/src/org/knopflerfish/service/remotefw/RemoteFramework.java
@@ -4,10 +4,51 @@ import org.osgi.framework.*;
 
 import java.util.Map;
 
+
+/**
+ * Service interface for remote framework connections.
+ *
+ * <p>
+ * By connecting to a remote framework, a client gets access
+ * to a <tt>BundleContext</tt> representing the entire remote
+ * platform. This BncleContext can be used as a normal context, 
+ * adding listeners, getting service references etc. 
+ * </p>
+ * <p>
+ * Some service instances
+ * as StartLevel and PackageAdmin may also be remotely accessible
+ * via the context. For services not remotely accessible, properties
+ * are still available (such as service id, object class array etc)
+ * </p>
+ */
 public interface RemoteFramework  {
 
+  /**
+   * Connect to a remote framework.
+   * 
+   * <p>
+   * The host string is implementation specific, but the SOAP
+   * implementation uses the form <tt>http://[host]:[port]</tt>
+   * </p>
+   * <p>
+   * If the connection fails, a RuntimeException is thrown.
+   * </p>
+   */
   public BundleContext connect(String host);
+
+  /**
+   * Disconnect from a previously connected framework.
+   *
+   * @param bc Context representing the remote framework. Must be
+   *           a context previously returned from <tt>connect</tt>
+   */
   public void disconnect(BundleContext bc);
 
+  /**
+   * Get system properties from a remote framework.
+   *
+   * @param bc Context representing the remote framework. Must be
+   *           a context previously returned from <tt>connect</tt>
+   */
   public Map getSystemProperties(BundleContext bc);
 }
