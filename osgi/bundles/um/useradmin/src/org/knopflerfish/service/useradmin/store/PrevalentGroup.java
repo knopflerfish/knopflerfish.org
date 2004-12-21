0c376a2e71e4c7b23c8aef28e68ce22b9fdc3103
erodriguez <erodriguez@87908148-3fd6-0310-a573-b04dead93e22>
2004-12-21 05:01:07 +0000

    New useradmin branch store package.

---
+++ osgi/bundles/um/useradmin/src/org/knopflerfish/service/useradmin/store/PrevalentGroup.java
@@ -0,0 +1,31 @@
+package org.knopflerfish.service.useradmin.store;
+
+import org.knopflerfish.service.useradmin.*;
+import org.knopflerfish.service.useradmin.impl.*;
+
+import org.osgi.service.useradmin.*;
+
+public class PrevalentGroup implements MemberStore {
+
+	public void addMember(Group group, Role role) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addRequiredMember(Group group, Role role) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void removeMember(Group group, Role role) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setRoleListener(RoleListener roleListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
