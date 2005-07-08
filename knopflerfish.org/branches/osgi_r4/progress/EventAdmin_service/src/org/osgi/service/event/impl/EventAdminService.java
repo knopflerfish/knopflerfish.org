a9f25b7709a808044e58506efa164c493cd7a0e8
johnny.baveras <johnny.baveras@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-08 12:34:16 +0000

    Lagt till lyssnare för bundleEvent, serviceEvent, frameworkEvent samt logEntry

--- knopflerfish.org/branches/osgi_r4/progress/EventAdmin_service/src/org/osgi/service/event/impl/EventAdminService.java
+++ knopflerfish.org/branches/osgi_r4/progress/EventAdmin_service/src/org/osgi/service/event/impl/EventAdminService.java
@@ -16,16 +16,30 @@ package org.osgi.service.event.impl;
 import java.security.AccessControlException;
 
 import java.util.Calendar;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.LinkedList;
 
 import org.knopflerfish.service.log.LogRef;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+import org.osgi.framework.Constants;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
+import org.osgi.service.event.EventHandler;
 import org.osgi.service.event.TopicPermission;
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogListener;
+import org.osgi.service.log.LogReaderService;
 
 /**
  * Default implementation of the EventAdmin interface this is a singleton class
@@ -37,7 +51,8 @@ import org.osgi.service.event.TopicPermission;
  * 
  * @author Magnus Klack
  */
-public class EventAdminService implements EventAdmin {
+public class EventAdminService implements EventAdmin, 
+BundleListener, LogListener, ServiceListener, FrameworkListener{
     /** the local representation of the bundle context */
     private BundleContext bundleContext;
 
@@ -49,6 +64,9 @@ public class EventAdminService implements EventAdmin {
 
     /** variable holding the asynchronus send procedure */
     private QueueHandler queueHandlerAsynch;
+    
+    /** A hasbtable of eventhandlers and timestamps */
+    private static Hashtable eventHandlers = new Hashtable();
 
     /**
      * the constructor use this to create a new Event admin service.
@@ -56,11 +74,23 @@ public class EventAdminService implements EventAdmin {
      * @param context
      *            the BundleContext
      */
-    public EventAdminService(BundleContext context) {
+    public EventAdminService(BundleContext context){
         /* assign the context to the local variable */
         bundleContext = context;
         /* create the log */
         log = new LogRef(context);
+        /* Adds this class as a listener of bundle events */
+        bundleContext.addBundleListener(this);
+        /* Gets the service reference of the log reader service*/
+        ServiceReference sr = bundleContext.getServiceReference(LogReaderService.class.getName());
+        /* Claims the log reader service */
+        LogReaderService logReader = (LogReaderService)bundleContext.getService(sr);
+        /* Adds this class as a listener of log events */
+        logReader.addLogListener(this);
+        /* Adds this class as a listener of service events */
+        bundleContext.addServiceListener(this);
+        /* Adds this class as a listener of framework events */
+        bundleContext.addFrameworkListener(this);
         /* create the synchronus sender process */
         queueHandlerSynch = new QueueHandler();
         /* start the asynchronus sender */
@@ -212,6 +242,263 @@ public class EventAdminService implements EventAdmin {
         }
 
     }
+    /**
+     * A listener for events sent by bundles
+     * @author Johnny Bäverås
+     */
+    public void bundleChanged(BundleEvent bundleEvent){
+    	/* A dictionary to store properties in */
+    	Dictionary props = new Hashtable();
+    	/* The bundle included in the bundleEvent */
+    	Bundle bundle = bundleEvent.getBundle();
+    	/* The prefix of the topic of the event to be posted*/
+    	String topic = "org/osgi/framework/BundleEvent/";
+    	/* boolean determining whether the bundleEvent is of a known topic */
+    	boolean knownMessageType = true;
+    	/* Determining the suffix of the topic of the event to be posted */
+    	switch(bundleEvent.getType()){
+			case BundleEvent.INSTALLED:
+				topic += "INSTALLED";
+    		break;
+			case BundleEvent.STARTED:
+				topic += "STARTED";
+    		break;
+			case BundleEvent.STOPPED:
+				topic += "STOPPED";
+    		break;
+			case BundleEvent.UPDATED:
+				topic += "UPDATED";
+    		break;
+			case BundleEvent.UNINSTALLED:
+				topic += "UNINSTALLED";
+    		break;
+    		default:
+    			/* Setting the boolean to false if an unknown event arrives */
+    			knownMessageType = false;
+    		break;
+    /*		case BundleEvent.RESOLVED:
+				topic += "RESOLVED";
+    		break;
+			case BundleEvent.UNRESOLVED:
+				topic += "UNRESOLVED";
+    		break;
+    */
+    	}
+    	
+    	/* Stores the properties of the event in the dictionary, if the event is known */
+    	if(knownMessageType){
+    		props.put("event", bundleEvent);
+    		props.put("bundle.id", new Long(bundle.getBundleId()));
+    		props.put("bundle.symbolicName", bundle.getLocation());//osäker på denna
+    		props.put("bundle", bundle);
+    		/* Tries posting the event once the properties are set */
+    		try{
+    			postEvent(new Event(topic, props));
+    		}
+    		catch(Exception e){
+    		}
+    	}
+    	else{
+    		/* Logs an error if the event, which arrived, were of an unknown type */
+    		log.error("Recieved unknown message, discarding");
+    	}
+    }
+    /**
+     * A listener for entries in the log
+     * @author Johnny Bäverås
+     */
+    public void logged(LogEntry logEntry){
+    	/* A dictionary to store properties in */
+    	Dictionary props = new Hashtable();
+    	/* The bundle included in the bundleEvent */
+    	Bundle bundle = logEntry.getBundle();
+    	/* The prefix of the topic of the event to be posted*/
+    	String topic = "org/osgi/service/log/LogEntry/";
+    	/* Determining the suffix of the topic of the event to be posted */
+    	switch(logEntry.getLevel()){
+			case LogRef.LOG_ERROR:
+				topic += "LOG_ERROR";
+    		break;
+			case LogRef.LOG_WARNING:
+				topic += "LOG_WARNING";
+    		break;
+			case LogRef.LOG_INFO:
+				topic += "LOG_INFO";
+    		break;
+			case LogRef.LOG_DEBUG:
+				topic += "LOG_DEBUG";
+    		break;
+    		default:
+    			/* if an unknown event arrives, it should be logged as a LOG_OTHER event */
+    			topic += "LOG_OTHER";
+    		break;
+    	}
+    	
+    	/* Stores the properties of the event in the dictionary */
+		props.put("bundle.id", new Long(bundle.getBundleId()));
+		props.put("bundle.symbolicName", bundle.getLocation());//osäker på denna, ska bara sättas om den inte är null
+		props.put("bundle", bundle);
+		props.put("log.level", new Integer(logEntry.getLevel()));
+		props.put("message", logEntry.getMessage());
+		props.put("timestamp", new Long(logEntry.getTime()));
+		props.put("log.entry", logEntry);
+		
+		/* If the event contains an exception, further properties shall be set */
+		if(logEntry.getException() != null){
+			Throwable e = logEntry.getException();
+			props.put("exception.class", Throwable.class.getName());
+			props.put("exception.message", e.getMessage());
+			props.put("exception", e);
+		}
+		
+		/* If the event contains a service reference, further properties shall be set */
+		if(logEntry.getServiceReference() != null){
+			props.put("service", logEntry.getServiceReference());
+			props.put("service.id", Constants.SERVICE_ID);
+			props.put("service.objectClass", Constants.OBJECTCLASS);
+			
+			/* If service_pid returns a non-null value, further properties shall be set*/
+			if(Constants.SERVICE_PID != null){
+				props.put("service.pid", Constants.SERVICE_PID);
+			}
+
+		/* Tries posting the event once the properties are set */
+			try{
+				postEvent(new Event(topic, props));
+			}catch(Exception e){
+				System.err.println("Exception:" + e.getMessage());
+			}
+		}
+    }
+    /**
+     * TODO Fråga Pelle om objectClass, fel typ.. string vs string[]
+     * TODO Fråga pelle om konstanterna i bundleevent
+     */
+
+	/**
+	 * A listener for service events
+	 * @author Johnny Bäverås
+	 */
+	public void serviceChanged(ServiceEvent serviceEvent) {
+		
+		
+    	/* A dictionary to store properties in */
+    	Dictionary props = new Hashtable();
+    	/* The prefix of the topic of the event to be posted*/
+    	String topic = "org/osgi/framework/ServiceEvent/";
+    	/* boolean determining whether the bundleEvent is of a known topic */
+    	boolean knownMessageType = true;
+    	/* Determining the suffix of the topic gof the event to be posted */
+    	switch(serviceEvent.getType()){
+			case ServiceEvent.REGISTERED:
+				topic += "REGISTERED";
+				/* Fetches the service sending the event and determines whether its an eventHandler */
+				if(bundleContext.getService(serviceEvent.getServiceReference()) instanceof EventHandler){
+					/* Adds the service reference as a key in the hashtable of handlers, and the timestamp as value */
+					eventHandlers.put(serviceEvent.getServiceReference(), new Long(System.currentTimeMillis()));
+				}
+    		break;
+			case ServiceEvent.MODIFIED:
+				topic += "MODIFIED";
+    		break;
+			case ServiceEvent.UNREGISTERING:
+				topic += "UNREGISTERING";
+				/* Tries to removes the entry in the hashtable */
+				try{
+					eventHandlers.remove(serviceEvent.getServiceReference());
+				}catch(NullPointerException e){
+					log.error("Tried to unregister a service which was not registered");
+				}
+    		break;
+    		default:
+    			/* Setting the boolean to false if an unknown event arrives */
+    			knownMessageType = false;
+    		break;
+    	}
+    	System.out.println("EventADMIN: current listneners: " + eventHandlers.size());
+    	/* Stores the properties of the event in the dictionary, if the event is known */
+    	if(knownMessageType){		    		
+    		props.put("event", serviceEvent);
+    		props.put("service", serviceEvent.getServiceReference());
+    		props.put("service.pid", Constants.SERVICE_PID);
+    		props.put("service.id", Constants.SERVICE_ID);
+    		props.put("service.objectClass", Constants.OBJECTCLASS);
+    		
+    		/* Tries posting the event once the properties are set */
+    		try{
+    			postEvent(new Event(topic, props));
+    		}catch(Exception e){
+    			System.err.println("Exception:" + e.getMessage());
+    		}
+    	}
+    	else{
+    		/* Logs an error if the event, which arrived, were of an unknown type */
+    		log.error("Recieved unknown message, discarding");
+    	}
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.FrameworkListener#frameworkEvent(org.osgi.framework.FrameworkEvent)
+	 */
+	public void frameworkEvent(FrameworkEvent frameworkEvent) {
+    	/* A dictionary to store properties in */
+    	Dictionary props = new Hashtable();
+    	/* The bundle included in the bundleEvent */
+    	Bundle bundle = frameworkEvent.getBundle();
+    	/* The prefix of the topic of the event to be posted*/
+    	String topic = "org/osgi/framework/FrameworkEvent/";
+    	/* boolean determining whether the bundleEvent is of a known topic */
+		boolean knownMessageType = true;
+    	switch(frameworkEvent.getType()){
+			case FrameworkEvent.STARTED:
+				topic += "STARTED";
+    		break;
+			case FrameworkEvent.ERROR:
+				topic += "ERROR";
+    		break;
+			case FrameworkEvent.PACKAGES_REFRESHED:
+				topic += "PACKAGES_REFRESHED";
+    		break;
+			case FrameworkEvent.STARTLEVEL_CHANGED:
+				topic += "STARTLEVEL_CHANGED";
+    		break;
+    		default:
+    			/* Setting the boolean to false if an unknown event arrives */
+    			knownMessageType = false;
+    		break;
+    	}
+    	
+    	/* Stores the properties of the event in the dictionary, if the event is known */
+    	if(knownMessageType){		
+    		props.put("event", frameworkEvent);
+    		/* If the event contains a bundle, further properties shall be set */
+    		if(frameworkEvent.getBundle() != null){  			
+    			props.put("bundle.id",  new Long(bundle.getBundleId()));
+    			props.put("bundle.symbolicName", bundle.getLocation());
+    			props.put("bundle", bundle);      			
+    		}
+    		
+    		/* If the event contains an exception, further properties shall be set */
+    		if(frameworkEvent.getThrowable() != null){
+    			Throwable e = frameworkEvent.getThrowable();
+    			props.put("exception.class", Throwable.class.getName());
+    			props.put("exception.message", e.getMessage());
+    			props.put("exception", e);
+    		}
+    		
+    		/* Tries posting the event once the properties are set */
+    		try{
+    			postEvent(new Event(topic, props));
+    		}
+    		catch(Exception e){
+    			System.err.println("Exception:" + e.getMessage());
+    		}
+    	}
+    	else{
+    		log.error("Recieved unknown message, discarding");
+    	}
+	}
 
     /**
      * this class will send the events synchronus to the event handlers it will
