caa0cd7fe277a6e97cb72b41da3119920c769e85
magnus.klack <magnus.klack@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-14 09:45:11 +0000

    nomsg

--- progress/EventAdmin_service/src/org/osgi/service/event/impl/Activator.java
+++ progress/EventAdmin_service/src/org/osgi/service/event/impl/Activator.java
@@ -18,61 +18,56 @@ import java.util.Hashtable;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
-import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.event.EventAdmin;
 
 /**
- * The Activator class is the startup class for the
- * EventHandlerService.
+ * The Activator class is the startup class for the EventHandlerService.
  * 
  * @author Magnus Klack
  */
-public class Activator implements BundleActivator{
-	/** local representation of the bundlecontext */
-	private  BundleContext bundleContext;
-	/** the EventAdminService */
-	private EventAdmin eventAdmin;
-	/** the service id string */
-	final static String SERVICE_ID = "org.osgi.service.event.EventAdmin";
-	/** service registration variable */
-	private ServiceRegistration serviceRegistration;
-	
-	
-	
-	/**
-	 * Main entry for the service
-	 * 
-	 * @param 	context the BundleContext ,ie, the handle to 
-	 * 			the OSGi framework 
-	 */
-	public void start(BundleContext context) throws Exception {
-		/* assign the context variable to a local variable */
-		bundleContext=context;
-		/* create the event admin service */
-		eventAdmin = new EventAdminService(bundleContext);
-		/* create the hashtable */
-		Hashtable propsTable = new Hashtable();
-		/* add the Constant variable and the id to the Hashtable */
-		propsTable.put(Constants.SERVICE_PID,SERVICE_ID);
-		/* register the service to the framework */
-		serviceRegistration = bundleContext.registerService(
-				EventAdmin.class.getName(),eventAdmin,propsTable);
-		
-		/* print that the service is up */
-		System.out.println("EventAdmin Service is up\n");
-	}
+public class Activator implements BundleActivator {
+    /** local representation of the bundlecontext */
+    private BundleContext bundleContext;
 
-	/**
-	 * Stop the service
-	 * 
-	 * @param 	context the BundleContext ,ie, the handle to 
-	 * 			the OSGi framework 
-	 */
-	public void stop(BundleContext context) throws Exception {
-		synchronized(eventAdmin){
-			eventAdmin=null;
-		}
-		
-	}
+    /** the EventAdminService */
+    private EventAdmin eventAdmin;
+
+    /** the service id string */
+    final static String SERVICE_ID = "org.osgi.service.event.EventAdmin";
+
+    /**
+     * Main entry for the service
+     * 
+     * @param context
+     *            the BundleContext ,ie, the handle to the OSGi framework
+     */
+    public void start(BundleContext context) throws Exception {
+        /* assign the context variable to a local variable */
+        bundleContext = context;
+        /* create the event admin service */
+        eventAdmin = new EventAdminService(bundleContext);
+        /* create the hashtable */
+        Hashtable propsTable = new Hashtable();
+        /* add the Constant variable and the id to the Hashtable */
+        propsTable.put(Constants.SERVICE_PID, SERVICE_ID);
+        /* register the service to the framework */
+        bundleContext.registerService(EventAdmin.class.getName(), eventAdmin,
+                propsTable);
+
+        /* print that the service is up */
+        System.out.println("EventAdmin Service is up\n");
+    }
+
+    /**
+     * Stop the service
+     * 
+     * @param context
+     *            the BundleContext ,ie, the handle to the OSGi framework
+     */
+    public void stop(BundleContext context) throws Exception {
+    	eventAdmin = null;
+        
+
+    }
 
 }
