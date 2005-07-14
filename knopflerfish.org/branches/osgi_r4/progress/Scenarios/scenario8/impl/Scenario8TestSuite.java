8cd97706bfc3f1bfd4898f3582f1d4bcd401583e
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-14 15:27:48 +0000

    Fixed the code a little, minor adjusments only

--- knopflerfish.org/branches/osgi_r4/progress/Scenarios/scenario8/impl/Scenario8TestSuite.java
+++ knopflerfish.org/branches/osgi_r4/progress/Scenarios/scenario8/impl/Scenario8TestSuite.java
@@ -13,17 +13,8 @@
  */
 package com.gstm.test.eventadmin.scenarios.scenario8.impl;
 
-import java.util.Calendar;
-import java.util.Hashtable;
-
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventAdmin;
-import org.osgi.service.event.EventConstants;
-import org.osgi.service.event.EventHandler;
 
 import com.gstm.test.eventadmin.scenarios.scenario8.Scenario8;
 import com.gstm.test.eventadmin.scenarios.util.Util;
@@ -32,17 +23,24 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
+ * This test check the ServicePermission and the TopicPermission 
+ * functionality
  * 
- * Resources needed 
- * EventConsumer1 - no ServicePermission and no TopicPermission
- * EventConsumer2 - no TopicPermission 
- * EventConsumer3 - no ServicePermission
- * EventConsumer4
+ * Resources needed:
+ * The following jar files needs to be placed in the framework.jar file 
+ * under testlibs/
+ * Also a policy file need to be constructed that follow the pattern
+ * below.
+ *   
+ * EventConsumer1.jar - no ServicePermission and no TopicPermission
+ * EventConsumer2.jar - no TopicPermission 
+ * EventConsumer3.jar - no ServicePermission
+ * EventConsumer4.jar
  * 
- * EventProducer1 - no ServicePermission and no TopicPermission 
- * EventProducer2 - no TopicPermission 
- * EventProducer3 - no ServicePermission 
- * EventProducer4
+ * EventProducer1.jar - no ServicePermission and no TopicPermission 
+ * EventProducer2.jar - no TopicPermission 
+ * EventProducer3.jar - no ServicePermission 
+ * EventProducer4.jar
  * 
  * @author Martin Berg
  *  
@@ -51,10 +49,9 @@ public class Scenario8TestSuite extends TestSuite implements Scenario8 {
     /** bundle context variable */
     BundleContext bundleContext;
     
-    Bundle buU;
+    /* A bundle variable*/
+    //Bundle buU;
     
-    String test_url_base;
-
     /**
 	 * Constructor for the TestSuite class.
 	 * 
@@ -140,7 +137,6 @@ public class Scenario8TestSuite extends TestSuite implements Scenario8 {
         public ExternalJAR(BundleContext bc, String name, int id, String path) {
             /* call super class */
             super(name + ":" + id);
-            /*assign the instance name */
             
             /* assign the instance bc */
             bundleContext = bc;
@@ -152,229 +148,8 @@ public class Scenario8TestSuite extends TestSuite implements Scenario8 {
         public void runTest() throws Throwable {
         	/* install the bundle */
         	
-        	buU = Util.installBundle(bundleContext, pathToJAR);
+        	Bundle buU = Util.installBundle(bundleContext, pathToJAR);
         	buU.start();
-        }
-    }    
-    
-//    class ExternalJAR extends TestCase{
-//    	/* The bundle xontext */
-//    	BundleContext bundleContext;
-//    	/* The path to the jar file to be installed*/
-//    	String pathToJAR;
-//        public ExternalJAR(BundleContext bc, String name, int id, 
-//        		String path) {
-//            /* call super class */
-//            super(name + ":" + id);
-//            /*assign the instance name */
-//            
-//            /* assign the instance bc */
-//            bundleContext = bc;
-//            /* assign the instance path */
-//            pathToJAR = path;
-//        }
-//        
-//        public void runTest() throws Throwable {
-//        	/* install the bundle */
-//        	buU = bundleContext.installBundle(pathToJAR);
-//        	System.out.print("Starting:" + getName() + "\n");
-//        	System.out.print("The BundleID:" + buU.getBundleId() + " State:" + buU.getState() + "\n");
-//        	System.out.print("The BundleID:" + buU.getLocation()+ "\n");
-//        	System.out.print("Services in use:" + buU.getServicesInUse().toString() + "\n");
-//        	buU.start();
-//        	System.out.print("The BundleID:" + buU.getBundleId() + " State:" + buU.getState() + "\n");
-//        	System.out.print("Services in use:" + buU.getServicesInUse().toString() + "\n");
-//        }
-//    }    
-//    
-    class EventPublisher extends TestCase {
-        
-           /** A reference to a service */
-           private ServiceReference serviceReference;
-
-           /** The admin which delivers the events */
-           private EventAdmin eventAdmin;
-
-           /** A calendar used to get the system time */
-           private Calendar calendar;
-
-           /** a variable indicating if the publisher is running */
-           private boolean running;
-
-           /** class variable holding bundle context */
-           private BundleContext bundleContext;
-
-           /** variable holding the topic to use */
-           private String topicToSend;
-           
-           public EventPublisher(BundleContext context, String name, int id,
-                   String topic) {
-               /* call super class */
-               super(name + ":" + id);
-               /* assign bundleContext */
-               bundleContext = context;
-               /* assign topic */
-               topicToSend = topic;
-               /* assign localCopy */
-           }
-
-           public void runTest() throws Throwable {
-           	/* determine if to use a local copy of EventAdmin or not */
-   			/* Claims the reference of the EventAdmin Service */
-   			serviceReference = bundleContext
-   					.getServiceReference(EventAdmin.class.getName());
-
-   			/* assert that a reference is aquired */
-   			assertNotNull(getName()
-   					+ " Should be able to get reference to EventAdmin service",
-   					serviceReference);
-
-   			if (serviceReference == null) {
-   				fail(getName() + " service reference should not be null");
-   			}
-
-   			eventAdmin = (EventAdmin) bundleContext
-   					.getService(serviceReference);
-
-   			assertNotNull(getName()
-   					+ " Should be able to get instance to EventAdmin object");
-
-   			if (eventAdmin == null) {
-   				fail(getName() + " event admin should not be null");
-   			}	
-    
-               Thread synchDeliver = new Thread() {
-               	public void run() {
-               		int i = 0;
-                   	while ( true){ 
-                   		
-                   			/* a Hash table to store message in */
-                               Hashtable message = new Hashtable();
-                               /* put some properties into the messages */
-                               message.put("Synchronus message",new Integer(i));
-                               /* send the message */
-                               System.out.println(getName() + " sending a synchronus event with message:" + 
-                               		message.toString() + "and the topic:" + topicToSend);
-                               eventAdmin.sendEvent(new Event(topicToSend, message));
-                   			/* Puts the thread to sleep for 10 seconds */
-                               i++;
-//                               try {
-//                   			//Thread.sleep(1000);
-//                   		} 
-//                   		catch( InterruptedException e ) {
-//                   			//ignored, treated by while loop
-//                   		}
-                   	}
-                   }
-               };
-
-               synchDeliver.start();
-
-               Thread asynchDeliver = new Thread() {
-                   public void run() {
-                   	int i = 0;
-                   	while ( ! Thread.interrupted()){ 
-                   		try {
-                   			/* a Hash table to store message in */
-                               Hashtable message = new Hashtable();
-                               /* put some properties into the messages */
-                               message.put("Asynchronus message",new Integer(i));
-                               /* send the message */
-                               System.out.println(getName() + " sending a synchronus event with message:" + message.toString() + "and the topic:" + topicToSend);
-                               eventAdmin.sendEvent(new Event(topicToSend, message));
-                   			/* Puts the thread to sleep for 10 seconds */
-                               i++;
-                   			Thread.sleep(10000);
-                   		} 
-                   		catch( InterruptedException e ) {
-                   			//ignored, treated by while loop
-                   		}
-                   	}
-                   }
-               };
-               //asynchDeliver.start();
-
-           }
-       }
-
-    class EventConsumer extends TestCase implements EventHandler {
-        /** class variable for service registration */
-        private ServiceRegistration serviceRegistration;
-
-        /** class variable indicating the instance name */
-        private int instanceId;
-
-        /** class variable indicating the topics correct version */
-        private String[] topicsToConsume;
-             
-        /** class variable keeping number of asynchronus message */
-        private int asynchMessages=0;
-        
-        /** class variable keeping number of asynchronus message */
-        private int synchMessages=0;
-             
-        /**
-		 * Constructor creates a consumer service
-		 * 
-		 * @param bundleContext
-		 * @param topics
-		 */
-        public EventConsumer(BundleContext bundleContext, String[] topics,
-        		String name, int id) {
-            /* call super class */
-            super(name + ":" + id);
-            /* assign the instance id */
-            instanceId = id;
-            /* assign the consume topics */
-            topicsToConsume = topics;
-        }
-        
-        public void runTest() throws Throwable {
-            /* create the hashtable to put properties in */
-            Hashtable props = new Hashtable();
-			/* determine what topicType to use */
-			/* put service.pid property in hashtable */
-			props.put(EventConstants.EVENT_TOPIC, topicsToConsume);
-                   
-            /* register the service */
-            serviceRegistration = bundleContext.registerService(
-                    EventHandler.class.getName(), this, props);
-
-            assertNotNull(getName()
-                    + " service registration should not be null",
-                    serviceRegistration);
-
-            if (serviceRegistration == null) {
-                fail("Could not get Service Registration ");
-            }
-        }
-
-        /**
-		 * This method takes events from the event admin service.
-		 */
-        public void handleEvent(Event event) {
-            //System.out.println(getName() + " recived an event");
-           
-            Object message;
-            /* try to get the message */
-            message = event.getProperty("Synchronus message");
-            
-            if(message != null){
-                /* its an asyncronous message */
-                synchMessages++;
-                
-                System.out.println(getName() + " recived an Synchronus event with message:" + message.toString());
-                
-            }else{
-              message = event.getProperty("Asynchronus message");
-              if(message!=null){
-                  asynchMessages++;
-                  System.out.println(getName() + " recived an Asynchronus event with message:" + message.toString());
-              }
-            }
-            
-            /* assert that the messages property is not null */
-            assertNotNull("Message should not be null in handleEvent()",message);
         }
     }    
 }
