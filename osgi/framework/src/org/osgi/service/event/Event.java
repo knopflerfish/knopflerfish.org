521754b0657c84db753507b7fb7350f993928892
johnny.baveras <johnny.baveras@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-19 08:26:36 +0000

    Updated interfaces

--- osgi/framework/src/org/osgi/service/event/Event.java
+++ osgi/framework/src/org/osgi/service/event/Event.java
@@ -1,10 +1,10 @@
 /*
  * $Header: /cvshome/build/org.osgi.service.event/src/org/osgi/service/event/Event.java,v 1.4 2005/05/13 20:33:29 hargrave Exp $
- * 
+ *
  * Copyright (c) OSGi Alliance (2005). All Rights Reserved.
- * 
+ *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this 
+ * terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html.
  */
 
@@ -16,15 +16,15 @@ import org.osgi.framework.Filter;
 
 /**
  * An event.
- * 
+ *
  * <p>
  * <code>Event</code> objects are delivered to <code>EventHandler</code>
  * services which subsrcibe to the topic of the event.
- * 
+ *
  * <p>
  * <i>NOTE: Although it is permitted to subclass <code>Event</code>, the
  * operations defined by this class MUST NOT be overridden. </i>
- * 
+ *
  * @version $Revision: 1.4 $
  */
 public class Event {
@@ -40,10 +40,10 @@ public class Event {
 
 	/**
 	 * Constructs an event.
-	 * 
+	 *
 	 * @param topic The topic of the event.
 	 * @param properties The event's properties (may be <code>null</code>).
-	 * 
+	 *
 	 * @throws IllegalArgumentException If topic is not a valid topic name.
 	 */
 	public Event(String topic, Dictionary properties) {
@@ -62,9 +62,9 @@ public class Event {
 
 	/**
 	 * Retrieves a property.
-	 * 
+	 *
 	 * @param name the name of the property to retrieve
-	 * 
+	 *
 	 * @return The value of the property, or <code>null</code> if not found.
 	 */
 	public final Object getProperty(String name) {
@@ -73,7 +73,7 @@ public class Event {
 
 	/**
 	 * Returns a list of this event's property names.
-	 * 
+	 *
 	 * @return A non-empty array with one element per property.
 	 */
 	public final String[] getPropertyNames() {
@@ -87,7 +87,7 @@ public class Event {
 
 	/**
 	 * Returns the topic of this event.
-	 * 
+	 *
 	 * @return The topic of this event.
 	 */
 	public final String getTopic() {
@@ -96,23 +96,24 @@ public class Event {
 
 	/**
 	 * Tests this event's properties against the given filter.
-	 * 
+	 *
 	 * @param filter The filter to test.
-	 * 
+	 *
 	 * @return true If this event's properties match the filter, false
 	 *         otherwise.
 	 */
 	public final boolean matches(Filter filter) {
-		return filter.matchCase(properties);
+	//	return filter.matchCase(properties);
+		return filter.match(properties);
 	}
 
 	/**
 	 * Compares this <code>Event</code> object to another object.
-	 * 
+	 *
 	 * <p>
 	 * An event is considered to be <b>equal to </b> another
 	 * event if the topic is equal and the properties are equal.
-	 * 
+	 *
 	 * @param object The <code>Event</code> object to be compared.
 	 * @return <code>true</code> if <code>object</code> is a
 	 *         <code>Event</code> and is equal to this object;
@@ -133,7 +134,7 @@ public class Event {
 
 	/**
 	 * Returns a hash code value for the object.
-	 * 
+	 *
 	 * @return An integer which is a hash code value for this object.
 	 */
 	public int hashCode() {
@@ -142,7 +143,7 @@ public class Event {
 
 	/**
 	 * Returns the string representation of this event.
-	 * 
+	 *
 	 * @return The string representation of this event.
 	 */
 	public String toString() {
@@ -153,7 +154,7 @@ public class Event {
 
 	/**
 	 * Called by the constructor to validate the topic name.
-	 * 
+	 *
 	 * @throws IllegalArgumentException If the topic name is invalid.
 	 */
 	private void validateTopicName() {
@@ -176,7 +177,7 @@ public class Event {
 
 	/**
 	 * Validate a token.
-	 * 
+	 *
 	 * @throws IllegalArgumentException If the token is invalid.
 	 */
 	private void validateToken(String token) {
