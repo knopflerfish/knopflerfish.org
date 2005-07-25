2fe009ecd22b082e8233611bdc6e472ce6ba3f09
magnus.klack <magnus.klack@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-25 12:12:26 +0000

    Added MyValueClient after example

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/MyValueClient/com/gstm/scr/MyValueClient.java
@@ -0,0 +1,41 @@
+/*
+ * @(#)MyValueClient.java        1.0 2005/06/28
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package com.gstm.scr;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/** 
+ * This class wil start a dummy client used 
+ * in the Declarative service feature within
+ * the OSGi framework.
+ */
+public class MyValueClient implements BundleActivator{
+
+	/**
+	 * Start the bundle 
+	 */
+	public void start(BundleContext context) throws Exception {
+		
+		
+	}
+
+	/**
+	 * Stop the bundle
+	 */
+	public void stop(BundleContext context) throws Exception {
+		
+		
+	}
+
+}
