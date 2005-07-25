2e13c5c44f8483d7206fdc2874d7ae7a34d82b2a
magnus.klack <magnus.klack@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-25 15:04:58 +0000

    Manifest updated

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/MyValueLocate/com/isv/process/myvalue/MyValueException.java
@@ -0,0 +1,27 @@
+/*
+ * @(#)MyValueException.java        1.0 2005/06/28
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package com.isv.process.myvalue;
+
+/**
+ * @author magnus
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class MyValueException extends Exception {
+	public MyValueException(String errMsg){
+		super(errMsg);
+	}
+
+}
