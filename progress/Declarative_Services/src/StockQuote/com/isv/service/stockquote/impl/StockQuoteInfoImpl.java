f8d03c3f0acf0b23756e00f0cda772e07e595a7c
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-25 12:07:57 +0000

    Renamed

---
+++ progress/Declarative_Services/src/StockQuote/com/isv/service/stockquote/impl/StockQuoteInfoImpl.java
@@ -0,0 +1,10 @@
+package com.isv.service.stockquote.impl; 
+
+import com.isv.service.stockquote.*; 
+
+public class StockQuoteImpl implements StockQuote { 
+    public float getQuote(String symbol) { 
+	// here is where the interaction with business logic would go 
+	return 100.0f; 
+    } 
+}
