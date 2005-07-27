2f7c456ef9beab64a522c796a351137183d1cfb2
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-27 15:30:37 +0000

    Added internal classes ComponentInformation PropertyInformation PropertiesInformation ServiceInformation ReferenceInformation

--- progress/Declarative_Services/src/SCR/org/osgi/service/component/impl/SystemComponentRuntimeImpl.java
+++ progress/Declarative_Services/src/SCR/org/osgi/service/component/impl/SystemComponentRuntimeImpl.java
@@ -201,4 +201,367 @@ public class SystemComponentRuntimeImpl implements BundleListener{
 		}
 	}
 
+	/**
+	 * This class is used to store information of a specific component
+	 * It has four helper classes that supply additional information
+	 * these are:
+	 * PropertiesInformation
+	 * PropertyInformation
+	 * ServiceInformation
+	 * ReferenceInformation
+	 */
+	class ComponentInformation{
+		
+		/* The name of the component*/
+		private String componentName;
+		/* to use autoenable on the component or not*/
+		private boolean autoEnable;
+		/* to use a factory with the component or not */
+		private boolean factory;
+		/* the class that implements the interfaces that is listed in serviceInfo*/
+		private String implementation;
+		
+		/* A list containing all properties for the component*/
+		private ArrayList propertiesInfo;
+		/* A list containing all property for the component*/
+		private ArrayList propertyInfo;
+		/* A list containing all services for the component*/
+		private ArrayList serviceInfo;
+		/* A list containing all references for the component*/
+		private ArrayList referenceInfo;
+		
+		/*The constructor*/
+		ComponentInformation(){
+			propertiesInfo = new ArrayList();
+			propertyInfo = new ArrayList();
+			serviceInfo = new ArrayList();
+			referenceInfo = new ArrayList();			
+		}
+		
+		/**
+		 * @return Returns the autoEnable.
+		 */
+		public boolean isAutoEnable() {
+			return autoEnable;
+		}
+		/**
+		 * @param autoEnable The autoEnable to set.
+		 */
+		public void setAutoEnable(boolean autoEnable) {
+			this.autoEnable = autoEnable;
+		}
+		/**
+		 * @return Returns the componentName.
+		 */
+		public String getComponentName() {
+			return componentName;
+		}
+		/**
+		 * @param componentName The componentName to set.
+		 */
+		public void setComponentName(String componentName) {
+			this.componentName = componentName;
+		}
+		/**
+		 * @return Returns the factory.
+		 */
+		public boolean isFactory() {
+			return factory;
+		}
+		/**
+		 * @param factory The factory to set.
+		 */
+		public void setFactory(boolean factory) {
+			this.factory = factory;
+		}
+		/**
+		 * @return Returns the implementation.
+		 */
+		public String getImplementation() {
+			return implementation;
+		}
+		/**
+		 * @param implementation The implementation to set.
+		 */
+		public void setImplementation(String implementation) {
+			this.implementation = implementation;
+		}
+		/**
+		 * @return Returns the propertiesInfo.
+		 */
+		public ArrayList getPropertiesInfo() {
+			return propertiesInfo;
+		}
+		/**
+		 * @param propertiesInfo The propertiesInfo to set.
+		 */
+		public void setPropertiesInfo(ArrayList propertiesInfo) {
+			this.propertiesInfo = propertiesInfo;
+		}
+		/**
+		 * @return Returns the propertyInfo.
+		 */
+		public ArrayList getPropertyInfo() {
+			return propertyInfo;
+		}
+		/**
+		 * @param propertyInfo The propertyInfo to set.
+		 */
+		public void setPropertyInfo(ArrayList propertyInfo) {
+			this.propertyInfo = propertyInfo;
+		}
+		/**
+		 * @return Returns the referenceInfo.
+		 */
+		public ArrayList getReferenceInfo() {
+			return referenceInfo;
+		}
+		/**
+		 * @param referenceInfo The referenceInfo to set.
+		 */
+		public void setReferenceInfo(ArrayList referenceInfo) {
+			this.referenceInfo = referenceInfo;
+		}
+		/**
+		 * @return Returns the serviceInfo.
+		 */
+		public ArrayList getServiceInfo() {
+			return serviceInfo;
+		}
+		/**
+		 * @param serviceInfo The serviceInfo to set.
+		 */
+		public void setServiceInfo(ArrayList serviceInfo) {
+			this.serviceInfo = serviceInfo;
+		}
+		
+		/**
+		 * @return Returns an instance of PropertiesInformation.
+		 */
+		public PropertiesInformation getNewPropertiesIntance(){ 
+			PropertiesInformation props = new PropertiesInformation();
+			return props;
+		}
+		
+		/**
+		 * @return Returns an instance of PropertyInformation.
+		 */
+		public PropertyInformation getNewPropertyIntance(){ 
+			PropertyInformation prop = new PropertyInformation();
+			return prop;
+		}
+		
+		/**
+		 * @return Returns an instance of ServiceInformation.
+		 */
+		public ServiceInformation getNewServiceIntance(){ 
+			ServiceInformation serv = new ServiceInformation();
+			return serv;
+		}
+
+		/**
+		 * @return Returns an instance of ReferenceInformation.
+		 */
+		public ReferenceInformation getNewReferenceIntance(){ 
+			ReferenceInformation ref = new ReferenceInformation();
+			return ref;
+		}
+	}
+	
+	/**
+	 * This class is a helper classes that supply additional information
+	 * to the component class
+	 */
+	class ServiceInformation{
+		/* A list containing all interfaces that the component supplys*/
+		ArrayList componentInterfaces;
+		
+		ServiceInformation(){
+			componentInterfaces  = new ArrayList();
+		}
+		
+		/* insert a componentInterface in the list*/
+		public void instertInterface(String componentInterface){
+			componentInterfaces.add(componentInterface);
+		}
+	}
+	
+	/**
+	 * This class is a helper classes that supply additional information
+	 * to the component class
+	 */
+	class ReferenceInformation{
+		/*component reference name*/
+		String referenceName;
+		/*Java interface*/
+		String interfaceType;
+		/*The number of service wich will bind up to this component*/
+		String cardinality;
+		/*Indicate when this service may be activated and deactivated, (either static or dynamic) */
+		String policy;
+		/* Selection filter to be used when selecting a desired service*/
+		String target;
+		/* The name of the component method to call when to bind*/
+		String bind;
+		/* The name of the component method to call when to unbind*/
+		String unbind;
+				
+		/**
+		 * @return Returns the bind.
+		 */
+		public String getBind() {
+			return bind;
+		}
+		/**
+		 * @param bind The bind to set.
+		 */
+		public void setBind(String bind) {
+			this.bind = bind;
+		}
+		/**
+		 * @return Returns the cardinality.
+		 */
+		public String getCardinality() {
+			return cardinality;
+		}
+		/**
+		 * @param cardinality The cardinality to set.
+		 */
+		public void setCardinality(String cardinality) {
+			this.cardinality = cardinality;
+		}
+		/**
+		 * @return Returns the interfaceType.
+		 */
+		public String getInterfaceType() {
+			return interfaceType;
+		}
+		/**
+		 * @param interfaceType The interfaceType to set.
+		 */
+		public void setInterfaceType(String interfaceType) {
+			this.interfaceType = interfaceType;
+		}
+		/**
+		 * @return Returns the policy.
+		 */
+		public String getPolicy() {
+			return policy;
+		}
+		/**
+		 * @param policy The policy to set.
+		 */
+		public void setPolicy(String policy) {
+			this.policy = policy;
+		}
+		/**
+		 * @return Returns the referenceName.
+		 */
+		public String getReferenceName() {
+			return referenceName;
+		}
+		/**
+		 * @param referenceName The referenceName to set.
+		 */
+		public void setReferenceName(String referenceName) {
+			this.referenceName = referenceName;
+		}
+		/**
+		 * @return Returns the target.
+		 */
+		public String getTarget() {
+			return target;
+		}
+		/**
+		 * @param target The target to set.
+		 */
+		public void setTarget(String target) {
+			this.target = target;
+		}
+		/**
+		 * @return Returns the unbind.
+		 */
+		public String getUnbind() {
+			return unbind;
+		}
+		/**
+		 * @param unbind The unbind to set.
+		 */
+		public void setUnbind(String unbind) {
+			this.unbind = unbind;
+		}
+	}
+	
+	/**
+	 * This class is a helper classes that supply additional information
+	 * to the component class
+	 */
+	class PropertiesInformation{
+		/* bundle entry name*/
+		private String entry;
+		
+		/**
+		 * @return Returns the entry.
+		 */
+		public String getEntry() {
+			return entry;
+		}
+		/**
+		 * @param entry The entry to set.
+		 */
+		public void setEntry(String entry) {
+			this.entry = entry;
+		}
+	}
+	
+	/**
+	 * This class is a helper classes that supply additional information
+	 * to the component class
+	 */
+	class PropertyInformation{
+		/* Property name*/
+		private String name;
+		/* Property value*/
+		private String value;
+		/* Property type */
+		private String type;
+		
+		/**
+		 * @return Returns the name.
+		 */
+		public String getName() {
+			return name;
+		}
+		/**
+		 * @param name The name to set.
+		 */
+		public void setName(String name) {
+			this.name = name;
+		}
+		/**
+		 * @return Returns the type.
+		 */
+		public String getType() {
+			return type;
+		}
+		/**
+		 * @param type The type to set.
+		 */
+		public void setType(String type) {
+			this.type = type;
+		}
+		/**
+		 * @return Returns the value.
+		 */
+		public String getValue() {
+			return value;
+		}
+		/**
+		 * @param value The value to set.
+		 */
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
+
 }
