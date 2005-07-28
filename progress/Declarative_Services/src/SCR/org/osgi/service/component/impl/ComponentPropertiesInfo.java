92c2b824fc543ebc592023fa8fd14fdf8ba3f543
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-07-28 07:31:36 +0000

    Added ComponentController and ist helper classes.

---
+++ progress/Declarative_Services/src/SCR/org/osgi/service/component/impl/ComponentPropertiesInfo.java
@@ -0,0 +1,41 @@
+/*
+ * @(#)ComponentPropertiesInfo.java        1.0 2005/07/28
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package org.osgi.service.component.impl;
+
+/**
+ * @author Administrator
+ * 
+ * This class is a helper classes that supply additional information to the
+ * component class
+ */
+public class ComponentPropertiesInfo {
+
+	/* bundle entry name */
+	private String entry;
+
+	/**
+	 * @return Returns the entry.
+	 */
+	public String getEntry() {
+		return entry;
+	}
+
+	/**
+	 * @param entry
+	 *            The entry to set.
+	 */
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
+}
