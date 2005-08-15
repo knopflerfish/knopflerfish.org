45a957ab36367bb031c69a95129142ebb0f27c74
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-15 14:49:26 +0000

    The first Scenario

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario1Bundle1/src/com/gstm/test/scr/scenarios/scenario1/bundle1/impl/Scenario1Impl.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Aug 15, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.gstm.test.scr.scenarios.scenario1.bundle1.impl;
+
+import com.gstm.test.scr.scenarios.scenario1.bundle1.Scenario1;
+
+/**
+ * @author Martin
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Scenario1Impl implements Scenario1{
+	public int getValue(){
+		return 1234;
+	}
+}
