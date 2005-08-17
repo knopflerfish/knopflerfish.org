c65e6369c4a5ed83e33e4d4e0c478ac6e3e553de
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-17 07:47:57 +0000

    Scenario3

---
+++ progress/Declarative_Services/src/TestScenarios/Scenario3UserEvent/src/com/gstm/test/scr/scenarios/scenario3/UserEvent.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Aug 16, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.gstm.test.scr.scenarios.scenario3;
+
+/**
+ * @author Martin
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface UserEvent { 
+
+}
