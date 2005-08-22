b8872f3cda2c762b1f59137f56303f8d4a1c9bf4
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-22 07:21:53 +0000

    Hopefully, final adjustments to scenario3

--- knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario3UserLook/src/com/gstm/test/scr/scenarios/scenario3/userlook/impl/UserLookImpl.java
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario3UserLook/src/com/gstm/test/scr/scenarios/scenario3/userlook/impl/UserLookImpl.java
@@ -1,8 +1,15 @@
 /*
- * Created on Aug 16, 2005
+ * @(#)UserLookImpl.Java        1.0 2005/08/16
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
  */
 package com.gstm.test.scr.scenarios.scenario3.userlook.impl;
 
@@ -12,10 +19,9 @@ import com.gstm.test.scr.scenarios.scenario3.provider1.Provider1;
 import com.gstm.test.scr.scenarios.util.impl.WhiteboardImpl;
 
 /**
- * @author Martin
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * @author Martin Berg
+ * 
+ * A bundle with one component used in Scenario 3
  */
 public class UserLookImpl {
     private ComponentContext context; 
@@ -24,19 +30,16 @@ public class UserLookImpl {
     
     // component activator 
     public void activate(ComponentContext context) { 
-    	System.out.println("||||||||||||||||The activate has been executed||||||||||||||||");
     	this.context = context;
 		
-		provider = (Provider1) context.locateService("message");
+    	provider = (Provider1) context.locateService("message");
+		
 		System.out.println("Provider:" + provider);
+		
 		if(provider!=null){
 			int serviceValue = provider.getValue();
 			System.out.println("UserLookImpl - The provider has returned the value: " + serviceValue);
 			board.setValue("com.gstm.test.scr.scenarios.scenario3.userlook.impl.UserLookImpl", new Integer(serviceValue));
 		}
-    }
-    
-    public UserLookImpl(){
-
     }
 }
--- knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenarios-SCR/src/com/gstm/test/scr/scenarios/scenario3/impl/Scenario3TestSuite.java
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenarios-SCR/src/com/gstm/test/scr/scenarios/scenario3/impl/Scenario3TestSuite.java
@@ -1,19 +1,23 @@
 /*
- * Created on Aug 15, 2005
+ * @(#)Scenario3TestSuite.Java        1.0 2005/08/15
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
  */
 package com.gstm.test.scr.scenarios.scenario3.impl;
 
-import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
-import java.util.LinkedList;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
@@ -25,197 +29,267 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * @author Martin
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * @author Martin Berg
+ * 
+ * Check that a component that changes properties is restarted and that 
+ * the service ranking works correctly.
  */
 public class Scenario3TestSuite extends TestSuite implements Scenario3 {
 
-    /** bundle context variable */
-    private BundleContext bundleContext;
-    
-    /**
-     * Constuructor for the TestSuite class.
-     * 
-     * @param context
-     *            the handle to the frame work
-     */
-    public Scenario3TestSuite(BundleContext context) {
-        /* call super class */
-        super("Scenario 3");
-        /* assign the bundle context to glocal variable */
-        bundleContext = context;
-        /* add the setup */
-        addTest(new Setup());
-        
-        addTest(new Scenario3(context));
-        
-        /* add the clean up */
-        addTest(new Cleanup());
-    }
-    
-    /**
-     * Sets up neccessary environment
-     *
-     *@author Magnus Klack
-     */
-    class Setup extends TestCase {
-        public Setup(){
-          
-        }
-        public void runTest() throws Throwable {
-           
-        }
-        public String getName() {
-            String name = getClass().getName();
-            int ix = name.lastIndexOf("$");
-            if(ix == -1) {
-              ix = name.lastIndexOf(".");
-            }
-            if(ix != -1) {
-              name = name.substring(ix + 1);
-            }
-            return name;
-          }
-    }
-    
-    /**
-     * Clean up the test suite
-     * 
-     * @author Magnus Klack
-     */
-    class Cleanup extends TestCase {
-        public void runTest() throws Throwable {
-            
-        }
-        public String getName() {
-            String name = getClass().getName();
-            int ix = name.lastIndexOf("$");
-            if(ix == -1) {
-              ix = name.lastIndexOf(".");
-            }
-            if(ix != -1) {
-              name = name.substring(ix + 1);
-            }
-            return name;
-          }
-    }
-   
-    class Scenario3 extends TestCase implements EventHandler {
-        /** class variable for service registration */
-        private ServiceRegistration serviceRegistration;
-        
-        /** local variable holding the bundleContext */
-        private BundleContext bundleContext;
-        
-        /** the bundles that are needed in the test */
-        Bundle[] desiredBundles = new Bundle[4];
+	/** bundle context variable */
+	private BundleContext bundleContext;
 
-        /** the dummy bundle */
-        private Bundle provider1;
-        private Bundle provider2;
-        private Bundle userEvent;
-        private Bundle userLook;
+	/**
+	 * Constuructor for the TestSuite class.
+	 * 
+	 * @param context
+	 *            the handle to the frame work
+	 */
+	public Scenario3TestSuite(BundleContext context) {
+		/* call super class */
+		super("Scenario 3");
+		/* assign the bundle context to glocal variable */
+		bundleContext = context;
+		/* add the setup */
+		addTest(new Setup());
 
-        /**
-         * Consturctor to initialize a dummy bundle 
-         * @param context
-         */
-        public Scenario3(BundleContext context) {
-            super("Scenario3");
-            /* assign the context */
-            bundleContext = context;
-        }
-        
-        public void runTest() throws Throwable {
-        	/* create the hashtable to put properties in */
-            Hashtable props = new Hashtable();
-            /* put service.pid property in hashtable */
-            props.put(EventConstants.EVENT_TOPIC, "com/gstm/test/scr/scenarios/util/Whiteboard");
-            /* register the service */
-            serviceRegistration = bundleContext.registerService(
-                    EventHandler.class.getName(), this, props);
-            
-            // Create list containing expected events
-            LinkedList expectedEvents = new LinkedList();
-            expectedEvents.addLast(new Integer(1));
-            expectedEvents.addLast(new Integer(1));
-            expectedEvents.addLast(new Integer(2));
-            expectedEvents.addLast(new Integer(2));
-            expectedEvents.addLast(new Integer(1));
-            expectedEvents.addLast(new Integer(1));
-            
-            // Find expected bundles and start them in predetermined order
-            findBundles();
-            
-            /* Start Scenario3UserEvent */
-        	try{
-        		desiredBundles[0].start();
-        	}catch (Exception e){
-        		assertFalse("Couldn't start the bundle:" + e, true);
-        	} 
-        	
-        	/* Start Scenario3UserLook */
-        	try{
-        		desiredBundles[1].start();
-        	}catch (Exception e){
-        		assertFalse("Couldn't start the bundle:" + e, true);
-        	}
-        	
-        	/* Start Scenario3Provider1 */
-        	try{
-        		desiredBundles[2].start();
-        	}catch (Exception e){
-        		assertFalse("Couldn't start the bundle:" + e, true);
-        	}
-        	
-        	/* Start Scenario3Provider2 */
-        	try{
-        		desiredBundles[3].start();
-        	}catch (Exception e){
-        		assertFalse("Couldn't start the bundle:" + e, true);
-        	}
-        }
+		addTest(new TestStarter(context));
 
-        private void findBundles(){
-        	Bundle[] bundles = bundleContext.getBundles();
-        	
-        	for(int i=0; i<bundles.length ; i++){
-        		Bundle bundle = bundles[i];
-        		Dictionary headers = bundle.getHeaders();
-        		String name = (String) headers.get("Bundle-Name");
-        		//System.out.println("Found: "+ name);
-        		if(name.equals("Scenario3Provider1")){
-        			desiredBundles[2] = bundle;
-        			System.out.println("Adding: Scenario3Provider1");
-        		}else if(name.equals("Scenario3Provider2")){
-        			desiredBundles[3] = bundle;
-        			System.out.println("Adding: Scenario1Provider2");
-        		} else if(name.equals("Scenario3UserLook")){
-        			desiredBundles[1] = bundle;
-        			System.out.println("Adding: Scenario3UserLook");
-        		} else if(name.equals("Scenario3UserEvent")){
-        			desiredBundles[0] = bundle;
-        			System.out.println("Adding: Scenario3UserEvent");
-        		}
-        	}
-        }
-        
-        /* Do the state changes here */
-        private void stateMachine(int state){
-        	switch (state) {
-				case 1: 
-					// do something
+		/* add the clean up */
+		addTest(new Cleanup());
+	}
+
+	/**
+	 * Sets up neccessary environment
+	 * 
+	 * @author Magnus Klack
+	 */
+	class Setup extends TestCase {
+		public Setup() {
+
+		}
+
+		public void runTest() throws Throwable {
+
+		}
+
+		public String getName() {
+			String name = getClass().getName();
+			int ix = name.lastIndexOf("$");
+			if (ix == -1) {
+				ix = name.lastIndexOf(".");
+			}
+			if (ix != -1) {
+				name = name.substring(ix + 1);
+			}
+			return name;
+		}
+	}
+
+	/**
+	 * Clean up the test suite
+	 * 
+	 * @author Magnus Klack
+	 */
+	class Cleanup extends TestCase {
+		public void runTest() throws Throwable {
+
+		}
+
+		public String getName() {
+			String name = getClass().getName();
+			int ix = name.lastIndexOf("$");
+			if (ix == -1) {
+				ix = name.lastIndexOf(".");
+			}
+			if (ix != -1) {
+				name = name.substring(ix + 1);
+			}
+			return name;
+		}
+	}
+
+	class TestStarter extends TestCase implements EventHandler {
+		/** class variable for service registration */
+		private ServiceRegistration serviceRegistration;
+
+		/** local variable holding the bundleContext */
+		private BundleContext bundleContext;
+
+		/** the bundles that are needed in the test */
+		Bundle[] desiredBundles = new Bundle[4];
+
+		/** Event counter */
+		int eventCounter = 1;
+
+		/** the dummy bundle */
+		private Bundle provider1;
+
+		private Bundle provider2;
+
+		private Bundle userEvent;
+
+		private Bundle userLook;
+
+		/**
+		 * Consturctor to initialize a dummy bundle
+		 * 
+		 * @param context
+		 */
+		public TestStarter(BundleContext context) {
+			super("Scenario3");
+			/* assign the context */
+			bundleContext = context;
+		}
+
+		public synchronized void runTest() throws Throwable {
+			/* create the hashtable to put properties in */
+			Hashtable props = new Hashtable();
+			/* put service.pid property in hashtable */
+			String[] topic = { "com/gstm/test/scr/scenarios/util/Whiteboard" };
+			props.put(EventConstants.EVENT_TOPIC, topic);
+			/* register the service */
+			serviceRegistration = bundleContext.registerService(
+					EventHandler.class.getName(), this, props);
+
+			assertNotNull(getName()
+					+ " service registration should not be null",
+					serviceRegistration);
+
+			if (serviceRegistration == null) {
+				fail("Could not get Service Registration ");
+			}
+
+			synchronized (this) {
+				try {
+					System.out.println("Waiting");
+					wait(40);
+				} catch (Exception e) {
+					System.out.println("Couldn't wait:" + e);
+				}
+			}
+
+			// Find expected bundles and start them in predetermined order
+			findBundles();
+
+			/* Start Scenario3UserEvent */
+			try {
+				desiredBundles[0].start();
+			} catch (Exception e) {
+				assertFalse("Couldn't start the bundle:" + e, true);
+			}
+
+			/* Start Scenario3UserLook */
+			try {
+				desiredBundles[1].start();
+			} catch (Exception e) {
+				assertFalse("Couldn't start the bundle:" + e, true);
+			}
+
+			/* Start Scenario3Provider1 */
+			try {
+				desiredBundles[2].start();
+			} catch (Exception e) {
+				assertFalse("Couldn't start the bundle:" + e, true);
+			}
+
+			/* Start Scenario3Provider2 */
+			try {
+				desiredBundles[3].start();
+			} catch (Exception e) {
+				assertFalse("Couldn't start the bundle:" + e, true);
+			}
+		}
+
+		private void findBundles() {
+			Bundle[] bundles = bundleContext.getBundles();
+
+			for (int i = 0; i < bundles.length; i++) {
+				Bundle bundle = bundles[i];
+				Dictionary headers = bundle.getHeaders();
+				String name = (String) headers.get("Bundle-Name");
+				//System.out.println("Found: "+ name);
+				if (name.equals("Scenario3Provider1")) {
+					desiredBundles[2] = bundle;
+					System.out.println("Adding: Scenario3Provider1");
+				} else if (name.equals("Scenario3Provider2")) {
+					desiredBundles[3] = bundle;
+					System.out.println("Adding: Scenario1Provider2");
+				} else if (name.equals("Scenario3UserLook")) {
+					desiredBundles[1] = bundle;
+					System.out.println("Adding: Scenario3UserLook");
+				} else if (name.equals("Scenario3UserEvent")) {
+					desiredBundles[0] = bundle;
+					System.out.println("Adding: Scenario3UserEvent");
+				}
+			}
+		}
+
+		public void handleEvent(Event event) {
+			System.out.println("\nThis event was received: " + event.getTopic()
+					+ "\n");
+
+			int userEventValue = ((Integer) event.getProperty("com.gstm.test.scr.scenarios.scenario3.UserEventImpl")).intValue();
+			int userLookValue = ((Integer) event.getProperty("com.gstm.test.scr.scenarios.scenario3.UserLookImpl")).intValue();
+
+			//assertTrue("Only one value for each event, and no value should be 0",(userEventValue == 0 && userLookValue > 0) || (userEventValue > 0 && userLookValue == 0));
+			
+			switch (eventCounter) {
+			case 2: //
+				System.out.println("First or second message");
+				if (userEventValue == 0) {
+					if (userLookValue == 0) {
+						/* no value in neither userEventValue or userLookValue */ 
+						assertTrue("There needs to be a value in the event",
+								false);
+					}
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value", userLookValue,
+							1);
+				} else {
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value",
+							userEventValue, 1);
+				}
+				eventCounter++;
 				break;
-				case 2: 
-					// do something else
+			case 4:
+				/* After the change of a property in ServiceProvider1 */
+				System.out.println("Third or forth message");
+				if (userEventValue == 0) {
+					if (userLookValue == 0) {
+						assertTrue("There needs to be a value in the event",
+								false);
+					}
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value", userLookValue,
+							2);
+				} else {
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value",
+							userEventValue, 2);
+				}
+				eventCounter++;
+				break;
+			case 6:
+				/* After the stop of the ServiceProvider2 */
+				System.out.println("Fifth or sixth message");
+				if (userEventValue == 0) {
+					if (userLookValue == 0) {
+						assertTrue("There needs to be a value in the event",
+								false);
+					}
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value", userLookValue,
+							3);
+				} else {
+					/* check that the value received is the expected value */
+					assertEquals("Not the expected event value",
+							userEventValue, 3);
+				}
+				eventCounter++;
 				break;
 			}
-        }
-        
-		public void handleEvent(Event event) {
-			System.out.println("This event was received: " + event.getTopic());
-		}      
-    }
+		}
+	}
 }
