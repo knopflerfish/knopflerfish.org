719b5794313fb1d3981c3ca726c36f6a16160505
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-22 13:11:31 +0000

    scenario4

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenarios-SCR/src/com/gstm/test/scr/scenarios/scenario4/Scenario4.java
@@ -0,0 +1,11 @@
+/*
+ * Created on Aug 22, 2005
+ */
+package com.gstm.test.scr.scenarios.scenario4;
+
+/**
+ * @author Martin Berg
+ */
+public interface Scenario4 {
+
+}
---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenarios-SCR/src/com/gstm/test/scr/scenarios/scenario4/impl/Scenario4TestSuite.java
@@ -0,0 +1,229 @@
+/*
+ * @(#)Scenario3TestSuite.Java        1.0 2005/08/15
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package com.gstm.test.scr.scenarios.scenario4.impl;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+import com.gstm.test.scr.scenarios.scenario4.Scenario4;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author Martin Berg
+ * 
+ * Check that a component that changes properties is restarted and that the
+ * service ranking works correctly.
+ */
+public class Scenario4TestSuite extends TestSuite implements Scenario4 {
+
+	/** bundle context variable */
+	private BundleContext bundleContext;
+
+	/**
+	 * Constuructor for the TestSuite class.
+	 * 
+	 * @param context
+	 *            the handle to the frame work
+	 */
+	public Scenario4TestSuite(BundleContext context) {
+		/* call super class */
+		super("Scenario 4");
+		/* assign the bundle context to glocal variable */
+		bundleContext = context;
+		/* add the setup */
+		addTest(new Setup());
+
+		addTest(new TestStarter(context));
+
+		/* add the clean up */
+		addTest(new Cleanup());
+	}
+
+	/**
+	 * Sets up neccessary environment
+	 * 
+	 * @author Magnus Klack
+	 */
+	class Setup extends TestCase {
+		public Setup() {
+
+		}
+
+		public void runTest() throws Throwable {
+
+		}
+
+		public String getName() {
+			String name = getClass().getName();
+			int ix = name.lastIndexOf("$");
+			if (ix == -1) {
+				ix = name.lastIndexOf(".");
+			}
+			if (ix != -1) {
+				name = name.substring(ix + 1);
+			}
+			return name;
+		}
+	}
+
+	/**
+	 * Clean up the test suite
+	 * 
+	 * @author Magnus Klack
+	 */
+	class Cleanup extends TestCase {
+		public void runTest() throws Throwable {
+
+		}
+
+		public String getName() {
+			String name = getClass().getName();
+			int ix = name.lastIndexOf("$");
+			if (ix == -1) {
+				ix = name.lastIndexOf(".");
+			}
+			if (ix != -1) {
+				name = name.substring(ix + 1);
+			}
+			return name;
+		}
+	}
+
+	class TestStarter extends TestCase implements EventHandler {
+		/** class variable for service registration */
+		private ServiceRegistration serviceRegistration;
+
+		/** local variable holding the bundleContext */
+		private BundleContext bundleContext;
+
+		/** the bundles that are needed in the test */
+		Bundle[] desiredBundles = new Bundle[1];
+
+		/** Event counter */
+		int eventCounter = 1;
+
+		/**
+		 * Consturctor to initialize a dummy bundle
+		 * 
+		 * @param context
+		 */
+		public TestStarter(BundleContext context) {
+			super("Scenario3");
+			/* assign the context */
+			bundleContext = context;
+		}
+
+		public synchronized void runTest() throws Throwable {
+			/* create the hashtable to put properties in */
+			Hashtable props = new Hashtable();
+			/* put service.pid property in hashtable */
+			String[] topic = { "com/gstm/test/scr/scenarios/util/Whiteboard" };
+			props.put(EventConstants.EVENT_TOPIC, topic);
+			/* register the service */
+			serviceRegistration = bundleContext.registerService(
+					EventHandler.class.getName(), this, props);
+
+			assertNotNull(getName()
+					+ " service registration should not be null",
+					serviceRegistration);
+
+			if (serviceRegistration == null) {
+				fail("Could not get Service Registration ");
+			}
+
+			synchronized (this) {
+				try {
+					System.out.println("Scenario 4: Waiting......");
+					wait(40);
+				} catch (Exception e) {
+					System.out.println("Scenario 4: Couldn't wait:" + e);
+				}
+			}
+
+			// Find expected bundles and start them in predetermined order
+			findBundles();
+
+			/* Start Scenario4Bundle1 */
+			try {
+				desiredBundles[0].start();
+			} catch (Exception e) {
+				assertFalse("Couldn't start the bundle:" + e, true);
+			}
+		}
+
+		private void findBundles() {
+			Bundle[] bundles = bundleContext.getBundles();
+
+			for (int i = 0; i < bundles.length; i++) {
+				Bundle bundle = bundles[i];
+				Dictionary headers = bundle.getHeaders();
+				String name = (String) headers.get("Bundle-Name");
+				//System.out.println("Found: "+ name);
+				if (name.equals("Scenario4Bundle1")) {
+					desiredBundles[0] = bundle;
+					System.out.println("Adding: " + name);
+				}
+			}
+		}
+
+		public void handleEvent(Event event) {
+
+			String immediateComponent2Name = "com.gstm.test.scr.scenarios.scenario4.immediatecomponent2.ImmediateComponent2";
+			String immediateComponent3Name = "com.gstm.test.scr.scenarios.scenario4.immediatecomponent3.ImmediateComponent3";
+
+			switch (eventCounter) {
+			case 1: //
+				assertNotNull(
+						"The first event should be from  ImmediateComponent2",
+						event.getProperty(immediateComponent2Name));
+
+				int immediate2Value = ((Integer) event
+						.getProperty(immediateComponent2Name)).intValue();
+
+				System.out.println("First event:immediate2Value:"
+						+ immediate2Value);
+
+				assertEquals("The value should be 1", immediate2Value, 1);
+				eventCounter++;
+				break;
+			case 2: //
+				assertNotNull(
+						"The second event should be from  ImmediateComponent3",
+						event.getProperty(immediateComponent3Name));
+
+				int immediate3Value = ((Integer) event
+						.getProperty(immediateComponent3Name)).intValue();
+
+				System.out.println("Second event:immediate3Value:"
+						+ immediate3Value);
+
+				assertEquals("The value should be 1", immediate3Value, 1);
+				eventCounter++;
+				break;
+			default:
+				System.out.println("No matching event");
+				assertTrue("No matching event", false);
+			}
+		}
+	}
+}
