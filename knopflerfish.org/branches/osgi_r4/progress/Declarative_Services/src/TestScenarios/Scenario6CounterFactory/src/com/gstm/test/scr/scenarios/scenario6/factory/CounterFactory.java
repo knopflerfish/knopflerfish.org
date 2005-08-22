4f89e362148f505daf69d1a258a37bb497b11e76
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-22 14:01:16 +0000

    Added bundles needed in Scenario6

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario6CounterFactory/src/com/gstm/test/scr/scenarios/scenario6/factory/CounterFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Aug 22, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.gstm.test.scr.scenarios.scenario6.factory;
+
+/**
+ * @author Martin
+ */
+public interface CounterFactory {
+	int getValue();
+}
