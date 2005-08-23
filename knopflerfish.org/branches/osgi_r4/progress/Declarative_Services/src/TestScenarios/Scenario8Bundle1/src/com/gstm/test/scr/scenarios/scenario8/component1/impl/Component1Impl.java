af6820bb5b816f24d9558205657ca279db27c138
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-23 12:04:48 +0000

    Added Scenario8

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario8Bundle1/src/com/gstm/test/scr/scenarios/scenario8/component1/impl/Component1Impl.java
@@ -0,0 +1,55 @@
+/*
+ * @(#)Component1Impl.Java        1.0 2005/08/22
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package com.gstm.test.scr.scenarios.scenario8.component1.impl;
+
+import org.osgi.service.component.ComponentContext;
+
+
+import com.gstm.test.scr.scenarios.scenario8.faultyprovider.FaultyProvider;
+import com.gstm.test.scr.scenarios.util.impl.WhiteboardImpl;
+
+/**
+ * @author Martin
+ */
+public class Component1Impl  {
+    private ComponentContext context; 
+    WhiteboardImpl board = WhiteboardImpl.INSTANCE;
+    FaultyProvider provider = null;
+    
+    public Component1Impl(){
+    	/* Notifying that the component has been instanciated */
+    	System.out.println("THE CONSTRUCTOR IN COMPONENT1 IS CALLED");
+    	board.setValue("com.gstm.test.scr.scenarios.scenario8.component1.Component1", new Integer(10));
+    }
+    
+    // component activator 
+    public void activate(ComponentContext context) { 
+    	this.context = context;
+    	
+    	try{
+    		provider = (FaultyProvider) context.locateService("message");
+   		}catch(ClassCastException e){
+   			System.err.println("error due to:\n" + e);
+   		}
+		
+		System.out.println("Provider:" + provider);
+		
+		if(provider!=null){
+			int serviceValue = provider.getValue();
+			System.out.println("Component1 getting values from faultyProvider:" + serviceValue);
+			/* Notifying that the component has received the service */
+			board.setValue("com.gstm.test.scr.scenarios.scenario8.component1.Component1", new Integer(serviceValue));
+		}
+    }
+}
---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario8Bundle2/src/com/gstm/test/scr/scenarios/scenario8/component2/impl/Component2Impl.java
@@ -0,0 +1,54 @@
+/*
+ * @(#)Component1Impl.Java        1.0 2005/08/22
+ *
+ * Copyright (c) 2003-2005 Gatespace telematics AB
+ * Otterhallegatan 2, 41670,Gothenburgh, Sweden.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of 
+ * Gatespace telematics AB. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Gatespace telematics AB.
+ */
+package com.gstm.test.scr.scenarios.scenario8.component2.impl;
+
+import org.osgi.service.component.ComponentContext;
+
+import com.gstm.test.scr.scenarios.scenario8.component2.Component2;
+import com.gstm.test.scr.scenarios.scenario8.faultyprovider.FaultyProvider;
+import com.gstm.test.scr.scenarios.util.impl.WhiteboardImpl;
+
+/**
+ * @author Martin
+ */
+public class Component2Impl implements Component2 {
+    private ComponentContext context; 
+    WhiteboardImpl board = WhiteboardImpl.INSTANCE;
+    FaultyProvider provider = null;
+    
+    public Component2Impl(){
+    	/* Notifying that the component has been instanciated */
+    	board.setValue("com.gstm.test.scr.scenarios.scenario8.component2.Component2", new Integer(10));
+    }
+    
+    // component activator 
+    public void activate(ComponentContext context) { 
+    	this.context = context;
+		
+    	try{
+    		provider = (FaultyProvider) context.locateService("message");
+   		}catch(ClassCastException e){
+   			System.err.println("error due to:\n" + e);
+   		}
+		
+		System.out.println("Provider:" + provider);
+		
+		if(provider!=null){
+			int serviceValue = provider.getValue();
+			System.out.println("Component1 getting values from faultyProvider:" + serviceValue);
+			/* Notifying that the component has received the service */
+			board.setValue("com.gstm.test.scr.scenarios.scenario8.component2.Component2", new Integer(serviceValue));
+		}
+    }
+}
