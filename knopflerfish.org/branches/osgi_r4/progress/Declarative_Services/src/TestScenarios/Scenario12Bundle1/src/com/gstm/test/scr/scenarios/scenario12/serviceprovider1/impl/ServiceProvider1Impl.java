aa979f17743d9a874e3e8c4e76b5447a9ac370f0
martin.berg <martin.berg@87908148-3fd6-0310-a573-b04dead93e22>
2005-08-24 11:35:03 +0000

    Added bundles needed in Scenario12

---
+++ knopflerfish.org/branches/osgi_r4/progress/Declarative_Services/src/TestScenarios/Scenario12Bundle1/src/com/gstm/test/scr/scenarios/scenario12/serviceprovider1/impl/ServiceProvider1Impl.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Aug 24, 2005
+ */
+package com.gstm.test.scr.scenarios.scenario12.serviceprovider1.impl;
+
+import com.gstm.test.scr.scenarios.scenario12.serviceprovider1.ServiceProvider1;
+
+/**
+ * @author Martin
+ */
+public class ServiceProvider1Impl implements ServiceProvider1 {
+
+	public int getValue() {
+		return 1;
+	}
+
+}
