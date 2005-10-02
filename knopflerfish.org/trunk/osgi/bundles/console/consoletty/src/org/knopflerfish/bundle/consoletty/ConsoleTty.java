88124da6033cb39b6acf730d7f39a319b24dabe7
ar <ar@87908148-3fd6-0310-a573-b04dead93e22>
2005-10-02 09:04:27 +0000

    Cleaned up and formatted with Eclipse built in Java conventions. Added project files used by KF eclipse plugin.

--- knopflerfish.org/trunk/osgi/bundles/console/consoletty/src/org/knopflerfish/bundle/consoletty/ConsoleTty.java
+++ knopflerfish.org/trunk/osgi/bundles/console/consoletty/src/org/knopflerfish/bundle/consoletty/ConsoleTty.java
@@ -50,228 +50,241 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
-import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-//  ********************     ConsoleTty     ********************
+// ******************** ConsoleTty ********************
 /**
- ** Bundle activator implementation.
- **
- ** @author Jan Stein
- ** @version $Revision: 1.1.1.1 $
+ * * Bundle activator implementation. * *
+ * 
+ * @author Jan Stein *
+ * @version $Revision: 1.1.1.1 $
  */
-public class ConsoleTty implements BundleActivator, ManagedService, ServiceTrackerCustomizer {
+public class ConsoleTty implements BundleActivator, ManagedService,
+        ServiceTrackerCustomizer {
 
-  final static private String logServiceName = LogService.class.getName();
-  final static private String consoleServiceName = ConsoleService.class.getName();
+    final static private String logServiceName = LogService.class.getName();
 
-  final static private String NONBLOCKING = "nonblocking";
-  private boolean nonblocking = false;
+    final static private String consoleServiceName = ConsoleService.class
+            .getName();
 
-  private ServiceTracker consoleTracker;
-  private Session consoleSession = null;
-  private BundleContext  bc;
+    final static private String NONBLOCKING = "nonblocking";
 
-  private Reader reader;
-  private PrintWriter writer;
+    boolean nonblocking = false;
 
-  /*---------------------------------------------------------------------------*
-   *			  BundleActivator implementation
-   *---------------------------------------------------------------------------*/
+    private ServiceTracker consoleTracker;
 
-  //  ====================    start      ====================
-  /**
-   ** Called by the framework when this bundle is started.
-   **
-   ** @param bc Bundle context.
-   ** @exception BundleException shold be thrown when implemented
-   */
-  public void start(BundleContext bc) throws Exception {
-    this.bc = bc;
+    private Session consoleSession = null;
 
-    log(LogService.LOG_INFO, "Starting");
+    private BundleContext bc;
 
-    // Get config
-    Hashtable p = new Hashtable();
-    p.put(Constants.SERVICE_PID,getClass().getName());
-    bc.registerService(ManagedService.class.getName(), this, p);
+    private Reader reader;
 
-    PrintStream out = null;
-    try {
-      ServiceReference[] srl =
-        bc.getServiceReferences(PrintStream.class.getName(),
-                               "(service.pid=java.lang.System.out)");
-      if(srl != null && srl.length == 1) {
-        out = (PrintStream)bc.getService(srl[0]);
-      }
-    } catch (Exception e) {
-      e.printStackTrace(System.out);
-    }
-    if(out == null) {
-      out = System.out;
-    }
+    private PrintWriter writer;
 
-    reader = new InputStreamReader(new SystemIn(bc));
-    writer = new PrintWriter(out);
+    /*---------------------------------------------------------------------------*
+     *			  BundleActivator implementation
+     *---------------------------------------------------------------------------*/
 
-    consoleTracker = new ServiceTracker(bc, consoleServiceName, this);
-    consoleTracker.open();
-  }
+    // ==================== start ====================
+    /**
+     * * Called by the framework when this bundle is started. * *
+     * 
+     * @param bc
+     *            Bundle context. *
+     * @exception BundleException
+     *                shold be thrown when implemented
+     */
+    public void start(BundleContext bc) throws Exception {
+        this.bc = bc;
 
-  //  ====================    stop      ====================
-  /**
-   ** Called by the framework when this bundle is stopped.
-   **
-   ** @param bc Bundle context.
-   */
-  public synchronized void stop(BundleContext bc) {
-    log(LogService.LOG_INFO, "Stopping");
-    consoleTracker.close();
-    try {
-      if (consoleSession != null) {
-	consoleSession.close();
-      }
-    } catch (Exception e) {
-      log(LogService.LOG_ERROR, "Failed to close session", e);
-    }
-  }
+        log(LogService.LOG_INFO, "Starting");
 
-  /*---------------------------------------------------------------------------*
-   *			  ManagedService implementation
-   *---------------------------------------------------------------------------*/
+        // Get config
+        Hashtable p = new Hashtable();
+        p.put(Constants.SERVICE_PID, getClass().getName());
+        bc.registerService(ManagedService.class.getName(), this, p);
 
-  //  ====================    updated     ====================
-  /**
-   ** Called by CM when it got this bundles configuration.
-   **
-   ** @param cfg contains the new configuration properties.
-   */
-  public synchronized void updated(Dictionary cfg)
-    throws ConfigurationException, IllegalArgumentException {
-    if (cfg != null) {
-      Boolean b = (Boolean)cfg.get(NONBLOCKING);
-      if (b != null) {
-        nonblocking = b.booleanValue();
-      }
-    } else {
-      nonblocking = false;
-    }
-  }
+        PrintStream out = null;
+        try {
+            ServiceReference[] srl = bc.getServiceReferences(PrintStream.class
+                    .getName(), "(service.pid=java.lang.System.out)");
+            if (srl != null && srl.length == 1) {
+                out = (PrintStream) bc.getService(srl[0]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        if (out == null) {
+            out = System.out;
+        }
 
-  /*---------------------------------------------------------------------------*
-   *			  ServiceTrackerCustomizer implementation
-   *---------------------------------------------------------------------------*/
+        reader = new InputStreamReader(new SystemIn(bc));
+        writer = new PrintWriter(out);
 
-  public Object addingService(ServiceReference reference) {
-    ConsoleService console = (ConsoleService) bc.getService(reference);
-    try {
-      consoleSession = console.runSession("console tty", reader, writer);
-    } catch (IOException ioe) {
-      log(LogService.LOG_ERROR, "Failed to start console session, can not continue");
-    }
-    return console;
-  }
-
-  public void modifiedService(ServiceReference reference, Object service) { }
-
-  public void removedService(ServiceReference reference, Object service) {
-    if (consoleSession != null) {
-      consoleSession.close();
-      consoleSession = null;
-    }
-  }
-
-  /*---------------------------------------------------------------------------*
-   *			  Protected utility methods
-   *---------------------------------------------------------------------------*/
-
-  //  ====================    log      ====================
-  /**
-   ** Utility method used for logging.
-   **
-   ** @param level Log level
-   ** @param msg   Log message
-   */
-  public void log(int level, String msg) {
-    log(level, msg, null);
-  }
-  public void log(int level, String msg, Exception e) {
-    ServiceReference srLog = bc.getServiceReference(logServiceName);
-    if (srLog != null) {
-      LogService sLog = (LogService)bc.getService(srLog);
-      if (sLog != null) {
-	if(e == null) {
-	  sLog.log(level, msg);
-	} else {
-	  sLog.log(level, msg, e);
-	}
-      }
-     bc.ungetService(srLog);
-    }
-  }
-
-  /* ------------------------------------------------------------
-     Help class for VMs that when blocking in read() om System.in
-     block other threads too.
-     ------------------------------------------------------------*/
-  class SystemIn extends InputStream {
-
-    InputStream in;
-
-    SystemIn(BundleContext bc) {
-      try {
-	ServiceReference[] srl =
-	  bc.getServiceReferences(InputStream.class.getName(),
-				 "(service.pid=java.lang.System.in)");
-	if(srl != null && srl.length == 1) {
-	  in = (InputStream)bc.getService(srl[0]);
-	}
-      } catch (Exception e) {
-	e.printStackTrace(System.err);
-      }
-      if(in == null) {
-	in = System.in;
-      }
+        consoleTracker = new ServiceTracker(bc, consoleServiceName, this);
+        consoleTracker.open();
     }
 
-
-    public void close() throws IOException {
-      in.close();
+    // ==================== stop ====================
+    /**
+     * * Called by the framework when this bundle is stopped. * *
+     * 
+     * @param bc
+     *            Bundle context.
+     */
+    public synchronized void stop(BundleContext bc) {
+        log(LogService.LOG_INFO, "Stopping");
+        consoleTracker.close();
+        try {
+            if (consoleSession != null) {
+                consoleSession.close();
+            }
+        } catch (Exception e) {
+            log(LogService.LOG_ERROR, "Failed to close session", e);
+        }
     }
 
-    public int available() throws IOException {
-      return in.available();
+    /*---------------------------------------------------------------------------*
+     *			  ManagedService implementation
+     *---------------------------------------------------------------------------*/
+
+    // ==================== updated ====================
+    /**
+     * * Called by CM when it got this bundles configuration. * *
+     * 
+     * @param cfg
+     *            contains the new configuration properties.
+     */
+    public synchronized void updated(Dictionary cfg)
+            throws IllegalArgumentException {
+        if (cfg != null) {
+            Boolean b = (Boolean) cfg.get(NONBLOCKING);
+            if (b != null) {
+                nonblocking = b.booleanValue();
+            }
+        } else {
+            nonblocking = false;
+        }
     }
 
-    public int read() throws IOException {
-      byte[] b = new byte[1];
-      if (read(b) == 1) {
-	return (int) b[0];
-      } else {
-	return -1;
-      }
+    /*---------------------------------------------------------------------------*
+     *			  ServiceTrackerCustomizer implementation
+     *---------------------------------------------------------------------------*/
+
+    public Object addingService(ServiceReference reference) {
+        ConsoleService console = (ConsoleService) bc.getService(reference);
+        try {
+            consoleSession = console.runSession("console tty", reader, writer);
+        } catch (IOException ioe) {
+            log(LogService.LOG_ERROR,
+                    "Failed to start console session, can not continue");
+        }
+        return console;
     }
 
-    public int read(byte[] b) throws IOException {
-      return read(b, 0, b.length);
+    public void modifiedService(ServiceReference reference, Object service) {
     }
 
-    public int read(byte[] buf, int off, int len) throws IOException {
-      if (nonblocking) {
-	int nap = 50;
-	while (in.available() == 0) {
-	  try {
-	    Thread.sleep(nap);
-	  } catch (InterruptedException e) {}
-	  nap = 200;
-	}
-      }
-      return in.read(buf, off, len);
+    public void removedService(ServiceReference reference, Object service) {
+        if (consoleSession != null) {
+            consoleSession.close();
+            consoleSession = null;
+        }
     }
-  }
+
+    /*---------------------------------------------------------------------------*
+     *			  Protected utility methods
+     *---------------------------------------------------------------------------*/
+
+    // ==================== log ====================
+    /**
+     * * Utility method used for logging. * *
+     * 
+     * @param level
+     *            Log level *
+     * @param msg
+     *            Log message
+     */
+    public void log(int level, String msg) {
+        log(level, msg, null);
+    }
+
+    public void log(int level, String msg, Exception e) {
+        ServiceReference srLog = bc.getServiceReference(logServiceName);
+        if (srLog != null) {
+            LogService sLog = (LogService) bc.getService(srLog);
+            if (sLog != null) {
+                if (e == null) {
+                    sLog.log(level, msg);
+                } else {
+                    sLog.log(level, msg, e);
+                }
+            }
+            bc.ungetService(srLog);
+        }
+    }
+
+    /*
+     * ------------------------------------------------------------ Help class
+     * for VMs that when blocking in read() om System.in block other threads
+     * too. ------------------------------------------------------------
+     */
+    class SystemIn extends InputStream {
+
+        InputStream in;
+
+        SystemIn(BundleContext bc) {
+            try {
+                ServiceReference[] srl = bc.getServiceReferences(
+                        InputStream.class.getName(),
+                        "(service.pid=java.lang.System.in)");
+                if (srl != null && srl.length == 1) {
+                    in = (InputStream) bc.getService(srl[0]);
+                }
+            } catch (Exception e) {
+                e.printStackTrace(System.err);
+            }
+            if (in == null) {
+                in = System.in;
+            }
+        }
+
+        public void close() throws IOException {
+            in.close();
+        }
+
+        public int available() throws IOException {
+            return in.available();
+        }
+
+        public int read() throws IOException {
+            byte[] b = new byte[1];
+            if (read(b) == 1) {
+                return b[0];
+            }
+            return -1;
+        }
+
+        public int read(byte[] b) throws IOException {
+            return read(b, 0, b.length);
+        }
+
+        public int read(byte[] buf, int off, int len) throws IOException {
+            if (nonblocking) {
+                int nap = 50;
+                while (in.available() == 0) {
+                    try {
+                        Thread.sleep(nap);
+                    } catch (InterruptedException e) {
+                    }
+                    nap = 200;
+                }
+            }
+            return in.read(buf, off, len);
+        }
+    }
 
 }
-
