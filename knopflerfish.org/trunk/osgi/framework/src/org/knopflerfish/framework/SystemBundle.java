199aa719fc2148d543bdd1156e1fa64fd04aef20
bjorna <bjorna@87908148-3fd6-0310-a573-b04dead93e22>
2005-11-21 15:18:22 +0000

    tab -> space

--- knopflerfish.org/trunk/osgi/framework/src/org/knopflerfish/framework/SystemBundle.java
+++ knopflerfish.org/trunk/osgi/framework/src/org/knopflerfish/framework/SystemBundle.java
@@ -67,7 +67,7 @@ public class SystemBundle extends BundleImpl {
   /**
    * Name of system property for exporting all J2SE 1.3 packages.
    */
-  private final static String EXPORT13 = 
+  private final static String EXPORT13 =
     "org.knopflerfish.framework.system.export.all_13";
 
   private HeaderDictionary headers = null;
@@ -96,11 +96,11 @@ public class SystemBundle extends BundleImpl {
     if("true".equals(System.getProperty(EXPORT13, "").trim())) {
       addSysPackagesFromFile(sp, "packages1.3.txt");
     }
-    
+
     addSysPackagesFromFile(sp, System.getProperty(SYSPKG_FILE, null));
-    
+
     addSystemPackages(sp);
-    
+
     bpkgs = new BundlePackages(this, sp.toString(), null, null);
     bpkgs.registerPackages();
     bpkgs.resolvePackages();
@@ -114,37 +114,37 @@ public class SystemBundle extends BundleImpl {
     String name = Bundle.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append(name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" + Framework.SPEC_VERSION);
-    
+              "=" + Framework.SPEC_VERSION);
+
     // Set up packageadmin package
     name = PackageAdmin.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append("," + name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" +  PackageAdminImpl.SPEC_VERSION);
-    
+              "=" +  PackageAdminImpl.SPEC_VERSION);
+
     // Set up permissionadmin package
     name = PermissionAdmin.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append("," + name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" +  PermissionAdminImpl.SPEC_VERSION);
-    
+              "=" +  PermissionAdminImpl.SPEC_VERSION);
+
     // Set up startlevel package
     name = StartLevel.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append("," + name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" +  StartLevelImpl.SPEC_VERSION);
-    
+              "=" +  StartLevelImpl.SPEC_VERSION);
+
     // Set up tracker package
     name = ServiceTracker.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append("," + name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" +  "1.2");
-    
+              "=" +  "1.2");
+
     // Set up URL package
     name = org.osgi.service.url.URLStreamHandlerService.class.getName();
     name = name.substring(0, name.lastIndexOf('.'));
     sp.append("," + name + ";" + Constants.PACKAGE_SPECIFICATION_VERSION +
-	      "=" +  "1.0");
+              "=" +  "1.0");
   }
 
 
@@ -156,29 +156,29 @@ public class SystemBundle extends BundleImpl {
     if(sysPkgFile != null) {
       File f = new File(sysPkgFile);
       if(!f.exists() || !f.isFile()) {
-	throw new RuntimeException("System package file '" + sysPkgFile + 
-				   "' does not exists");
+        throw new RuntimeException("System package file '" + sysPkgFile +
+                                   "' does not exists");
       } else {
-	if(Debug.packages) {
-	  Debug.println("adding system packages from file " + sysPkgFile);
-	}
-	BufferedReader in = null;
-	try {
-	  in = new BufferedReader(new FileReader(sysPkgFile));
-	  String line;
-	  for(line = in.readLine(); line != null; 
-	      line = in.readLine()) {
-	    line = line.trim();
-	    if(line.length() > 0 && !line.startsWith("#")) {
-	      sp.append(line);
-	      sp.append(",");
-	    }
-	  } 
-	} catch (IOException e) {
-	  throw new IllegalArgumentException("Failed to read " + sysPkgFile + ": " + e);
-	} finally {
-	  try {   in.close();  } catch (Exception ignored) { }
-	}
+        if(Debug.packages) {
+          Debug.println("adding system packages from file " + sysPkgFile);
+        }
+        BufferedReader in = null;
+        try {
+          in = new BufferedReader(new FileReader(sysPkgFile));
+          String line;
+          for(line = in.readLine(); line != null;
+              line = in.readLine()) {
+            line = line.trim();
+            if(line.length() > 0 && !line.startsWith("#")) {
+              sp.append(line);
+              sp.append(",");
+            }
+          }
+        } catch (IOException e) {
+          throw new IllegalArgumentException("Failed to read " + sysPkgFile + ": " + e);
+        } finally {
+          try {   in.close();  } catch (Exception ignored) { }
+        }
       }
     }
   }
@@ -239,8 +239,8 @@ public class SystemBundle extends BundleImpl {
     framework.checkAdminPermission();
     throw new BundleException("uninstall of System bundle is not allowed");
   }
-    
-    
+
+
   /**
    * Get header data. Simulate EXPORT-PACKAGE.
    *
