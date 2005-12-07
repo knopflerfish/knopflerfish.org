b1094a7f581ab048ee11efc907ebaaaac6d7cdc7
philippe <philippe@87908148-3fd6-0310-a573-b04dead93e22>
2005-12-07 14:06:20 +0000

    copyright merge

--- osgi/framework/src/org/osgi/framework/AdminPermission.java
+++ osgi/framework/src/org/osgi/framework/AdminPermission.java
@@ -32,11 +32,23 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.osgi.framework;
-
 /**
  * @author Philippe Laporte
  */
+
+/* 
+ * Copyright (c) OSGi Alliance (2005). All Rights Reserved.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this 
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * This copyright applies to the comments and public final static String fields only
+ */
+
+package org.osgi.framework;
+
+
 
 import java.security.*;
 
