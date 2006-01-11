5f12eadd3588281948b40750f1b7b7edf8b186f2
polli <polli@87908148-3fd6-0310-a573-b04dead93e22>
2006-01-11 13:08:13 +0000

    The most fundamental parts of the (new) Declarative Service.

---
+++ osgi/bundles/component/src/org/knopflerfish/bundle/component/Config.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2005, KNOPFLERFISH project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials
+ *   provided with the distribution.
+ *
+ * - Neither the name of the KNOPFLERFISH project nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.knopflerfish.bundle.component;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+import org.osgi.framework.Bundle;
+
+public class Config {
+    
+  private String name;
+  private String implementation;
+  private String factory;
+
+  boolean enabled;
+  boolean serviceFactory;
+  boolean immediate;
+
+  private Hashtable properties;
+  private ArrayList references;
+  private ArrayList services;
+  private ArrayList components;
+  
+  private Bundle bundle;
+  
+  public Config(Bundle bundle) {
+    this.bundle = bundle;
+    
+    properties = new Hashtable();
+    references = new ArrayList();
+    services   = new ArrayList();
+  }
+
+  /*
+  public Component createComponent() {
+    Component retval;
+    if (immediate) {
+      retval = new ImmediateComponent(this);
+    } else if (serviceFactory) {
+      retval = new FactoryComponent(this);
+    } else {
+      retval = new DelayedComponent(this);
+    }
+    
+    components.add(retval);
+  } 
+  */
+  public boolean isSatisfied() {
+
+    for (int i = 0; i < references.size(); i++) {
+      Reference ref = (Reference)references.get(i);
+      
+      if (!ref.isSatisfied())
+	return false;
+    }
+    
+    return true;
+  }
+
+  public String[] getServices() {
+    String[] ret = new String[services.size()];
+    services.toArray(ret);
+    return ret;
+  }
+  
+  public Dictionary getProperties() {
+    return properties;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public String getImplementation() {
+    return implementation;
+  }
+
+  public boolean isImmediate() {
+    return immediate;
+  }
+
+  public boolean isServiceFactory() {
+    return serviceFactory;
+  }
+
+  public String getFactory() {
+    return factory;
+  }
+  
+  /* this are only for the parser. */
+  public void setProperty(String key, Object value) {
+    properties.put(key, value);
+  }
+  
+  public void addReference(Reference ref) {
+    references.add(ref);
+  }
+
+  public void addService(String interfaceName) {
+    services.add(interfaceName);
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+  
+  public void setImplementation(String impl) {
+    implementation = impl;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setServiceFactory(boolean isServiceFactory) {
+    serviceFactory = isServiceFactory;
+  }
+
+  public void setFactory(String factory) {
+    this.factory = factory;
+  }
+
+  public void setImmediate(boolean isImmediate) {
+    immediate = isImmediate;
+  }
+}
---
+++ osgi/bundles/component/src/org/knopflerfish/bundle/component/SCR.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2005, KNOPFLERFISH project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials
+ *   provided with the distribution.
+ *
+ * - Neither the name of the KNOPFLERFISH project nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.knopflerfish.bundle.component;
+
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import org.osgi.service.log.LogService;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+public class SCR{
+
+  private static ServiceTracker logTracker;
+
+
+  public SCR(BundleContext bc) {
+    logTracker = new ServiceTracker(bc,
+				    LogService.class.getName(), 
+				    null);
+    logTracker.open();
+
+    
+  }
+
+  public static void log(int level, String str) {
+    log(level, str, null);
+  }
+
+  public static void log(int level, String str, 
+			 Throwable throwable) {
+    
+    if (logTracker == null) {
+      // todo: this should be safe to remove.. 
+      // (was using it for testing the parser)
+      return ;
+    }
+
+    LogService service = (LogService)logTracker.getService();
+
+    if (service == null)
+      return ;
+
+    service.log(level, str, throwable);
+  }
+
+  
+
+}
