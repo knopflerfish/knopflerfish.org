8e9e57d4de1736bca6387f0ae58e25051e6fcbce
jan <jan@87908148-3fd6-0310-a573-b04dead93e22>
2006-05-30 15:36:18 +0000

    Fixed problem with validation of configuration dictionaries

--- knopflerfish.org/branches/kf_r4/osgi/bundles/cm/cm/src/org/knopflerfish/bundle/cm/Activator.java
+++ knopflerfish.org/branches/kf_r4/osgi/bundles/cm/cm/src/org/knopflerfish/bundle/cm/Activator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2005, KNOPFLERFISH project
+ * Copyright (c) 2003-2006, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -62,16 +62,9 @@ public class Activator implements BundleActivator {
 
     static ConfigurationAdminFactory configAdminFactory = null;
 
-    static boolean R3_TESTCOMPLIANT = false;
-
 
     public void start(BundleContext bc) {
         Activator.bc = bc;
-
-        R3_TESTCOMPLIANT = "true".equals(System.getProperty(
-                "org.knopflerfish.osgi.r3.testcompliant", "false")
-                .toLowerCase());
-
         throwIfBundleContextIsNull();
         createLogRef();
         createAndRegisterConfigurationAdminFactory();
@@ -129,7 +122,4 @@ public class Activator implements BundleActivator {
         }
     }
 
-    static boolean r3TestCompliant() {
-        return R3_TESTCOMPLIANT;
-    }
 }
