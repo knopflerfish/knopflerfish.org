343ce3e9f0fe524db9e709b6787a8c60271f80fd
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2008-01-14 08:38:45 +0000

    Merge correction made be Per on some other branch

--- knopflerfish.org/branches/kf_1_support/osgi/framework/src/org/knopflerfish/framework/StartLevelImpl.java
+++ knopflerfish.org/branches/kf_1_support/osgi/framework/src/org/knopflerfish/framework/StartLevelImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, KNOPFLERFISH project
+ * Copyright (c) 2003-2008, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -214,7 +214,7 @@ public class StartLevelImpl implements StartLevel, Runnable {
             }
           }
           notifyFramework();
-          if (notifyWC) {
+          if (notifyWC && null!=wc) {
             synchronized (wc) {
               wc.notifyAll();
             }
