2dea1f3615a498b5626928f95ba941636a4226fc
ekolin <ekolin@makewave.com>
2008-01-30 16:57:01 +0000

    Update of nightly build tools. Now produces builds wiht a version like 2.0.4_2175, where the last part is the SVN revision that the build was made on.

--- tools/nightlybuild/build.sh
+++ tools/nightlybuild/build.sh
@@ -1,7 +1,10 @@
-
 #!/bin/sh
 
 /bin/date
 
-cd /home/wistrand/knopflerfish.org/build
-make all
+cd `dirname $0`
+
+# Remove old build result and update to the current level.
+gmake update
+# Start build (separate command to ensure that everything can be updated).
+gmake all
