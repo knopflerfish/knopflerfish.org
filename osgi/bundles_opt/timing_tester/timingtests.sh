edf83125b1b4ae6057f040e7350ebacc1afcd3d0
ekolin <ekolin@makewave.com>
2008-02-22 17:55:22 +0000

    Updated to work with KF 2.0.*

--- osgi/bundles_opt/timing_tester/timingtests.sh
+++ osgi/bundles_opt/timing_tester/timingtests.sh
@@ -1,33 +1,27 @@
 #!/usr/bin/bash -e
 
-JAVA=/c/j2sdk1.4.2_03/bin/java
+ANT=${ANT:-ant}
 LOGFILE=log.csv
 TOPDIR=../..
 
 function runtest() {
-    prefix=$1
-    jarfile=$2
-    storage=$3
+    VERSION=$1
+    FWJARFILE=$2
+    STORAGE=$3
 
-     $JAVA \
-	"-Dtest.log.prefix=$prefix" \
-        -Dtest.log.file=$LOGFILE \
-	-Dorg.knopflerfish.framework.bundlestorage=$storage \
-	-Dfwdir.delete=true \
-	-cp $TOPDIR/jars/tester/tester-1.0.0.jar\;$TOPDIR/$jarfile \
-	org.knopflerfish.test.framework.TestFW \
-	-xargs test.xargs
-  
+    "${ANT}" -Dlog.file.name=${LOGFILE} \
+        -Dfw.storage=${STORAGE} \
+        -Dfw.version=${VERSION} \
+        -Dfw.jar=${FWJARFILE} \
+        run
 }
 
 
 function runAll() {
 
   for n in 1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10; do
-   runtest "1.3_file, 1.3,  file"    "framework.jar" "file"
-#   runtest "1.3_mem, 1.0, memory"    "framework.jar" "memory"
-   runtest "1.0_file, 1.0, file"     "framework-1.0.2.jar" "file"
-#   runtest "1.0_mem, 1.0, memory"    "framework-1.0.2.jar" "memory"
+   runtest "2.0" "${TOPDIR}/framework.jar" "file"
+   runtest "2.0" "${TOPDIR}/framework.jar" "memory"
   done
 }
 
