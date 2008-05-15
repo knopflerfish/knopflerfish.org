70b895c67595f4ee2ca756c89a487bc97501527e
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2008-05-15 08:03:33 +0000

    Remove unwanted, extra quote level around the repository name.

--- knopflerfish.org/trunk/ant/src/org/knopflerfish/ant/taskdefs/bundle/BIndexTask.java
+++ knopflerfish.org/trunk/ant/src/org/knopflerfish/ant/taskdefs/bundle/BIndexTask.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2003-2008, KNOPFLERFISH project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the KNOPFLERFISH project nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.knopflerfish.ant.taskdefs.bundle;
 
 import java.io.File;
@@ -57,9 +89,6 @@ public class BIndexTask extends Task {
     bindexJar = s;
   }
 
-  // File -> BundleInfo
-  //Set jarMap = new HashSet();
-
   // Implements Task
   public void execute() throws BuildException {
     if (filesets.size() == 0) {
@@ -115,7 +144,7 @@ public class BIndexTask extends Task {
 
       if (null!=repoName && repoName.length()>0) {
         cmdList.add("-n");
-        cmdList.add("\""+repoName +"\"");
+        cmdList.add(repoName);
       }
       cmdList.add("-r");
       cmdList.add(outFile);
@@ -129,8 +158,10 @@ public class BIndexTask extends Task {
 
       String[] args = (String[]) cmdList.toArray(new String[cmdList.size()]);
       try {
-        // Do the follwoing call, with the field rootFile set to baseDir.
-        // org.osgi.impl.bundle.bindex.Index.main(args);
+        // Call org.osgi.impl.bundle.bindex.Index.main(args), but
+        // first set the value of the field
+        // org.osgi.impl.bundle.bindex.Index.rootFile to baseDir to
+        // get correctly computed paths in the bundle URLs.
 
         // Load the Index class and change the rootFile variable to
         // the specified baseDir. This is a hack to get correct
