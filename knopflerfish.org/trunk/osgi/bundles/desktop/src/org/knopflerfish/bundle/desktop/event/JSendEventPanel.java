0252a366f4c5320c701ec4e2ae414df5d7813a2b
wistrand <wistrand@87908148-3fd6-0310-a573-b04dead93e22>
2008-08-27 15:02:18 +0000

    fix of type handling in property events

--- knopflerfish.org/trunk/osgi/bundles/desktop/src/org/knopflerfish/bundle/desktop/event/JSendEventPanel.java
+++ knopflerfish.org/trunk/osgi/bundles/desktop/src/org/knopflerfish/bundle/desktop/event/JSendEventPanel.java
@@ -148,6 +148,12 @@ public class JSendEventPanel extends JPanel  {
           
           return getDefaultRenderer(val != null ? val.getClass() : String.class);    
         }
+        public TableCellEditor getCellEditor(int row, int column) {
+          Object val = getValueAt(row, column);
+          
+          TableCellEditor ce = getDefaultEditor(val != null ? val.getClass() : String.class);    
+          return ce;
+        }
       };
 
     makeModel(null);
@@ -216,6 +222,18 @@ public class JSendEventPanel extends JPanel  {
           return data[col].get(row);
         }
         public void setValueAt(Object val, int row, int col) { 
+          Object oldVal = data[col].get(row);
+          if(oldVal != null && val != null) {
+            if(oldVal.getClass() != val.getClass()) {
+              try {
+                Object val2 = oldVal.getClass()
+                  .getConstructor(new Class[] { String.class})
+                  .newInstance(new Object[] { val.toString() });
+                val = val2;
+              } catch (Exception e) {
+              }
+            }
+          }
           data[col].set(row, val);
         }
       };
@@ -242,6 +260,8 @@ public class JSendEventPanel extends JPanel  {
         }
       }
     }
+    props.put("timestamp.generated", new Long(System.currentTimeMillis()));
+
     org.osgi.service.event.Event ev = 
       new org.osgi.service.event.Event(topic, props);
 
