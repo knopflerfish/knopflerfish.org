0c02076ac7b56b5c985f193026a25d9d3cbb5705
wistrand <wistrand@87908148-3fd6-0310-a573-b04dead93e22>
2009-05-07 08:14:31 +0000

    fixed broken CSV generation

--- knopflerfish.org/trunk/osgi/bundles_opt/timing_tester/timingtests.sh
+++ knopflerfish.org/trunk/osgi/bundles_opt/timing_tester/timingtests.sh
@@ -19,7 +19,7 @@ function runtest() {
 
 function runAll() {
 
-  for runid in 1 2; do
+  for runid in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
    runtest $runid "${TOPDIR}/framework.jar" "file"
    runtest $runid "${TOPDIR}/framework.jar" "memory"
   done
