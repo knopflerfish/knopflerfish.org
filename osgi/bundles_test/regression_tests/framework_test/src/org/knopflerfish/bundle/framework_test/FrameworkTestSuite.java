0c477681a164401443cf902cd1eb817eae76b313
ekolin <ekolin@makewave.com>
2009-05-20 12:32:48 +0000

    More entries in the top dir of the framework_test bundle.

--- osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/FrameworkTestSuite.java
+++ osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/FrameworkTestSuite.java
@@ -1387,9 +1387,9 @@ public class FrameworkTestSuite extends TestSuite implements FrameworkTest {
       // framework_tests bundle is changed in such a way that new
       // files or directories are added or removed to / from the top
       // level of the bundle jar-file.
-      if(i != 43){
+      if(i != 46){
           fail("GetEntryPaths did not retrieve the correct number of elements, "
-               +"43 != "+ i);
+               +"46 != "+ i);
       }
 
       //another existing directory
