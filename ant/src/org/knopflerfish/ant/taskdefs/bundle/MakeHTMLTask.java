74ec59088eb7b7a971d3927c6424d096247925f6
cl <cl@makewave.com>
2009-06-10 11:44:53 +0000

    Addedd new htdocs replaces variables: BASE_VERSION, DISTRIB_NAME and RELEASE_NAME

--- ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
+++ ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
@@ -323,8 +323,14 @@ public class MakeHTMLTask extends Task {
                              System.getProperty("user.name"));
       content = Util.replace(content, "$(VERSION)",
                              proj.getProperty("version"));
+      content = Util.replace(content, "$(BASE_VERSION)",
+                             proj.getProperty("base_version"));
       content = Util.replace(content, "$(DISTNAME)",
                              proj.getProperty("distname"));
+      content = Util.replace(content, "$(DISTRIB_NAME)",
+                             proj.getProperty("distrib.name"));
+      content = Util.replace(content, "$(RELEASE_NAME)",
+                             proj.getProperty("release.name"));
       content = Util.replace(content, "$(MESSAGE)",
                              proj.getProperty("release"));
       content = Util.replace(content, "$(BUNDLE_LIST)", bundleList);
