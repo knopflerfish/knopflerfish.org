7bd27ab46615e9fc69aa7dd57c690916cd1a7b4f
ekolin <ekolin@makewave.com>
2009-07-07 15:01:47 +0000

    Adapt the jarunpacker to the new documentation path; documentation now lives in knoppflerfish.org/docs. Improve installation size presentation.

--- tools/jarunpacker/src/org/knopflerfish/tools/jarunpacker/Strings.java
+++ tools/jarunpacker/src/org/knopflerfish/tools/jarunpacker/Strings.java
@@ -45,14 +45,14 @@ import java.util.*;
  *
  * Conventions:
  * <pre>
- * button_xx      String for xx to be placed on a graphical button 
+ * button_xx      String for xx to be placed on a graphical button
  * menu_xx        String for xx to be placed in a window menu
  * tooltip_xx     String for tooltip xx text
  * str_xx         Generic string for xx
  * </pre>
  */
 public class Strings {
-  
+
   private static ResourceBundle rb     = null;
   private static boolean bDebug        = false;
   private static String  RESOURCE_NAME = "strings";
@@ -64,11 +64,11 @@ public class Strings {
   static ResourceBundle identityResourceBundle = new ResourceBundle() {
       Hashtable keys = new Hashtable();
       public Object handleGetObject(String key) {
-	keys.put(key, "");
-	return key;
+        keys.put(key, "");
+        return key;
       }
       public Enumeration getKeys() {
-	return  keys.keys();
+        return  keys.keys();
       }
   };
 
@@ -79,31 +79,31 @@ public class Strings {
 
     if(rb == null) {
       if(bDebug) {
-	System.out.println("loading resource " + RESOURCE_NAME + ", locale=" + 
-			   locale);
+        System.out.println("loading resource " + RESOURCE_NAME + ", locale=" +
+                           locale);
       }
       try {
-	if(locale == null) {
-	  rb = ResourceBundle.getBundle(RESOURCE_NAME);
-	} else {
-	  rb = ResourceBundle.getBundle(RESOURCE_NAME, locale);
-	}
-	if(bDebug) {
-	  System.out.println("loaded resource " + RESOURCE_NAME + 
-			     " and locale=" + locale);
-	  for(Enumeration e = rb.getKeys(); e.hasMoreElements(); ) {
-	    String key = (String)e.nextElement();
-	    System.out.println(key + " = '" + rb.getString(key) + "'");
-	  }
-	}
+        if(locale == null) {
+          rb = ResourceBundle.getBundle(RESOURCE_NAME);
+        } else {
+          rb = ResourceBundle.getBundle(RESOURCE_NAME, locale);
+        }
+        if(bDebug) {
+          System.out.println("loaded resource " + RESOURCE_NAME +
+                             " and locale=" + locale);
+          for(Enumeration e = rb.getKeys(); e.hasMoreElements(); ) {
+            String key = (String)e.nextElement();
+            System.out.println(key + " = '" + rb.getString(key) + "'");
+          }
+        }
       } catch (Exception e) {
-	System.out.println("Failed to load resources for " + RESOURCE_NAME +
-			   " and locale=" + locale);
+        System.out.println("Failed to load resources for " + RESOURCE_NAME +
+                           " and locale=" + locale);
       }
     }
     if(rb == null) {
       System.out.println("reverting to identity resource " +
-			 RESOURCE_NAME +" and locale=" + locale);
+                         RESOURCE_NAME +" and locale=" + locale);
       rb = identityResourceBundle;
     }
   }
@@ -115,11 +115,11 @@ public class Strings {
   static public String get(String key) {
     try {
       if(rb != null) {
-	return rb.getString(key);
+        return rb.getString(key);
       }
     } catch (Exception e) {
       System.out.println("Failed to get " + key + ": " + e);
-    } 
+    }
     return key;
   }
 
@@ -129,12 +129,12 @@ public class Strings {
   static public boolean exists(String key) {
     try {
       if(rb != null) {
-	String val = rb.getString(key);
-	return true;
+        String val = rb.getString(key);
+        return true;
       }
     } catch (Exception e) {
       // This means the key disn't exist
-    } 
+    }
     return false;
   }
 
@@ -152,28 +152,85 @@ public class Strings {
     String s = get(key);
     return splitwords(s, ", ");
   }
-  
+
+
+
+  /**
+   * Factor to use when computing binary bytes.
+   */
+  static final long binaryByteFactor = 1024;
+
+  /**
+   * Separator between the number and the unit.
+   */
+  static final String sep = " ";
+
+  /**
+   * The unit.
+   */
+  static final String unit = "B";
+
+  /**
+   * Format value using ki, Mi, Gi, ... using multiples of 1024. I.e.,
+   * binary "bytes".
+   *
+   * @param value the byte value to format.
+   */
+  public static String fmtByte(long value) {
+    String formatedValue = "";
+    String[] suffixes = new String[]{ "",  "Ki","Mi",
+                                      "Gi","Ti","Pi",
+                                      "Ei","Zi","Yi"};
+    int ix = 0;
+    long factor = 1;
+
+    while(value/factor>binaryByteFactor && ix<suffixes.length) {
+      factor *= binaryByteFactor;
+      ix++;
+    }
+
+    long i = value/factor;    // Integral part of reduced value
+    long r = value -i*factor; // Remainder.
+    // Convert r to a decimal fraction
+    double fraction  = ((double) r)/((double) factor);
+    formatedValue = formatValue(i, fraction);
+
+    return formatedValue +sep +suffixes[ix] +unit;
+  }
+
+  private static String formatValue( long integral, double fraction)
+  {
+    String res = String.valueOf(integral);
+    if (integral<10) { // Append two digits from the fraction
+      int dec = (int) (fraction*100);
+      res = res +"." +(dec<10?"0":"") +String.valueOf(dec);
+    } else if (integral<100) { // Append one digit from the fraction
+      int dec = (int) (fraction*10);
+      res = res +"." +String.valueOf(dec);
+    }
+    return res;
+  }
 
 
   /**
    * Format a string with argument.
    *
-   * @param key Key to lookup using <code>get(key)<code>. 
-   *            The resulting string is used as source string for 
+   * @param key Key to lookup using <code>get(key)<code>.
+   *            The resulting string is used as source string for
    *            argument substitution.
    *            <code>$(1)</code> is replaced with <code>arg1</code>
    * @param arg1 Replacement string.
    */
   public static String fmt(String key, Object arg1) {
-    return replace(get(key), "$(1)", 
-		   arg1 != null ? arg1.toString() : "null");
+    return replace(get(key), "$(1)",
+                   arg1 != null ? arg1.toString() : "null");
   }
 
   /**
    * Format a string with arguments.
    *
-   * @param key Key to lookup using <code>get(key)<code>. 
-   *            The resulting string is used as source string for 
+   * @param key Key to lookup using <code>get(key)<code>.
+   *            The resulting string is used as source string for
    *            argument substitution.
    *            <code>$(1)</code> is replaced with <code>arg1</code><br>
    *            <code>$(2)</code> is replaced with <code>arg2</code>
@@ -181,13 +238,13 @@ public class Strings {
    * @param arg2 Replacement string.
    */
   public static String fmt(String key, Object arg1, Object arg2) {
-    return replace(fmt(key, arg1), "$(2)", 
-		   arg2 != null ? arg2.toString() : "null");
+    return replace(fmt(key, arg1), "$(2)",
+                   arg2 != null ? arg2.toString() : "null");
   }
 
   public static String fmt(String key, Object arg1, Object arg2, Object arg3) {
-    return replace(fmt(key, arg1, arg2), "$(3)", 
-		   arg3 != null ? arg3.toString() : "null");
+    return replace(fmt(key, arg1, arg2), "$(3)",
+                   arg3 != null ? arg3.toString() : "null");
   }
 
 
@@ -196,25 +253,25 @@ public class Strings {
    *
    * @param s  Source string.
    * @param v1 String to be replaced with <code>v2</code>.
-   * @param v2 String replacing <code>v1</code>. 
+   * @param v2 String replacing <code>v1</code>.
    * @return Modified string. If any of the input strings are <tt>null</tt>,
    *         the source string will be returned unmodified.
    */
-  public static String replace(final String s, 
-			       final String v1, 
-			       final String v2) {
-    
+  public static String replace(final String s,
+                               final String v1,
+                               final String v2) {
+
     // return quick when nothing to do
-    if(s == null 
-       || v1 == null 
-       || v2 == null 
-       || v1.length() == 0 
+    if(s == null
+       || v1 == null
+       || v2 == null
+       || v1.length() == 0
        || v1.equals(v2)) {
       return s;
     }
 
     int ix       = 0;
-    int v1Len    = v1.length(); 
+    int v1Len    = v1.length();
     int n        = 0;
 
     // count number of occurances to be able to correctly size
@@ -239,15 +296,15 @@ public class Strings {
     while(-1 != (ix = s.indexOf(v1, start))) {
       while(start < ix) r[rPos++] = s.charAt(start++);
       for(int j = 0; j < v2Len; j++) {
-	r[rPos++] = v2.charAt(j);
+        r[rPos++] = v2.charAt(j);
       }
       start += v1Len;
     }
 
     // ...and add all remaining chars
-    ix = s.length(); 
+    ix = s.length();
     while(start < ix) r[rPos++] = s.charAt(start++);
-    
+
     // ..ouch. this hurts.
     return new String(r);
   }
@@ -257,20 +314,20 @@ public class Strings {
 
   /** citation char (<code>'"'</code>) for word grouping. Used by splitwords() */
   protected static char   CITCHAR    = '"';
-  
-  
+
+
   /**
-   * Split a string into words separated by whitespace 
+   * Split a string into words separated by whitespace
    * (<code>Text.WHITESPACE</code>).
    * <p>
-   * Citation chars <code>Text.CITCHAR</code> may be used to group words 
+   * Citation chars <code>Text.CITCHAR</code> may be used to group words
    * with embedded whitespace.
    * </p>
    */
   public static String [] splitwords(String s) {
     return splitwords(s, WHITESPACE);
   }
-  
+
   /**
    * Split a string into words separated by whitespace.
    * Citation chars '"' may be used to group words with embedded
@@ -285,45 +342,45 @@ public class Strings {
   public static String [] splitwords(String s, String whiteSpace) {
     boolean       bCit  = false;        // true when inside citation chars.
     Vector        v     = new Vector(); // (String) individual words after splitting
-    StringBuffer  buf   = null; 
-    int           i     = 0; 
-    
+    StringBuffer  buf   = null;
+    int           i     = 0;
+
     while(i < s.length()) {
       char c = s.charAt(i);
 
       if(bCit || whiteSpace.indexOf(c) == -1) {
-	// Build up word until we breaks on either a citation char or whitespace
-	if(c == CITCHAR) {
-	  bCit = !bCit;
-	} else {
-	  if(buf == null) {
-	    buf = new StringBuffer();
-	  }
-	  buf.append(c);
-	}
-	i++;
-      } else {	
-	// found whitespace or end of citation, append word if we have one
-	if(buf != null) {
-	  v.addElement(buf.toString());
-	  buf = null;
-	}
+        // Build up word until we breaks on either a citation char or whitespace
+        if(c == CITCHAR) {
+          bCit = !bCit;
+        } else {
+          if(buf == null) {
+            buf = new StringBuffer();
+          }
+          buf.append(c);
+        }
+        i++;
+      } else {
+        // found whitespace or end of citation, append word if we have one
+        if(buf != null) {
+          v.addElement(buf.toString());
+          buf = null;
+        }
 
-	// and skip whitespace so we start clean on a word or citation char
-	while((i < s.length()) && (-1 != whiteSpace.indexOf(s.charAt(i)))) {
-	  i++;
-	}
+        // and skip whitespace so we start clean on a word or citation char
+        while((i < s.length()) && (-1 != whiteSpace.indexOf(s.charAt(i)))) {
+          i++;
+        }
       }
     }
     // Add possible remaining word
     if(buf != null) {
       v.addElement(buf.toString());
     }
-    
+
     // Copy back into an array
     String [] r = new String[v.size()];
     v.copyInto(r);
-    
+
     return r;
   }
 
