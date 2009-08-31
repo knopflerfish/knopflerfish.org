240538c231ad45432cc0bc65f3933505426d5c66
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2009-08-31 09:07:17 +0000

    Added ServiceListener test case where the service registrar bundle uses Require-Bundle to access the service package. See Bug-tracker item with id 2844362 on SourceFourge.

--- knopflerfish.org/trunk/osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/ServiceListenerTestSuite.java
+++ knopflerfish.org/trunk/osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/ServiceListenerTestSuite.java
@@ -61,6 +61,7 @@ public class ServiceListenerTestSuite
   Bundle buSL1;
   Bundle buSL2;
   Bundle buSL3;
+  Bundle buSL4;
 
   PrintStream out = System.out;
   PrintStream err = System.err;
@@ -87,6 +88,7 @@ public class ServiceListenerTestSuite
     addTest(new FrameSL10a());
     addTest(new FrameSL15a());
     addTest(new FrameSL20a());
+    addTest(new FrameSL25a());
     addTest(new Cleanup());
   }
 
@@ -124,6 +126,8 @@ public class ServiceListenerTestSuite
       assertNotNull(buSL2);
       buSL3 = Util.installBundle(bc, "bundleSL3-1.0.0.jar");
       assertNotNull(buSL3);
+      buSL4 = Util.installBundle(bc, "bundleSL4-1.0.0.jar");
+      assertNotNull(buSL4);
       out.println("org.knopflerfish.servicereference.valid.during.unregistering"
                   +" is " +UNREGISTERSERVICE_VALID_DURING_UNREGISTERING);
       out.println("### ServiceListenerTestSuite :SETUP:PASS");
@@ -143,6 +147,7 @@ public class ServiceListenerTestSuite
         buSL1,
         buSL2,
         buSL3,
+        buSL4,
       };
       for(int i = 0; i < bundles.length; i++) {
         try {  bundles[i].uninstall();  }
@@ -154,6 +159,7 @@ public class ServiceListenerTestSuite
       buSL1 = null;
       buSL2 = null;
       buSL3 = null;
+      buSL4 = null;
     }
   }
 
@@ -203,10 +209,9 @@ public class ServiceListenerTestSuite
     extends FWTestCase
   {
     public void runTest() throws Throwable {
-      boolean teststatus = true;
       int cnt = 1;
 
-      teststatus = runStartStopTest( "FRAMEsl10A", cnt, buA2,
+      assertTrue(runStartStopTest( "FRAMEsl10A", cnt, buA2,
                                      new int[]{
                                        ServiceEvent.REGISTERED,
                                        ServiceEvent.UNREGISTERING,
@@ -214,15 +219,8 @@ public class ServiceListenerTestSuite
                                      new int[]{
                                        ServiceEvent.REGISTERED,
                                        ServiceEvent.UNREGISTERING,
-                                     });
-
-
-      if (teststatus == true) {
-        out.println("### ServiceListenerTestsuite :FRAMEsl10A:PASS");
-      }
-      else {
-        fail("### ServiceListenerTestsuite :FRAMEsl10A:FAIL");
-      }
+                                     }));
+      out.println("### ServiceListenerTestsuite :FRAMEsl10A:PASS");
     }
   }
 
@@ -242,24 +240,22 @@ public class ServiceListenerTestSuite
     extends FWTestCase
   {
     public void runTest() throws Throwable {
-      boolean teststatus = true;
-      int cnt = 1;
 
       ServiceListener sListen = new ServiceListener();
       try {
         bc.addServiceListener(sListen);
       } catch (IllegalStateException ise) {
-        teststatus  = false;
-        err.println("service listener registration failed "+ ise
-                    + " :FRAMEsl15A:FAIL");
+        err.println("service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+        fail("service listener registration failed "+ ise);
       }
       AllServiceListener asListen = new AllServiceListener();
       try {
         bc.addServiceListener(asListen);
       } catch (IllegalStateException ise) {
-        teststatus  = false;
-        err.println("all service listener registration failed "+ ise
-                    + " :FRAMEsl15A:FAIL");
+        err.println("all service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+        fail("all service listener registration failed "+ ise);
       }
 
       int[] expectedServiceEventTypes = new int[]{
@@ -546,24 +542,22 @@ public class ServiceListenerTestSuite
     extends FWTestCase
   {
     public void runTest() throws Throwable {
-      boolean teststatus = true;
-      int cnt = 1;
 
       ServiceListener sListen = new ServiceListener(false);
       try {
         bc.addServiceListener(sListen);
       } catch (IllegalStateException ise) {
-        teststatus  = false;
-        err.println("service listener registration failed "+ ise
-                    + " :FRAMEsl20A:FAIL");
+        err.println("service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+        fail("service listener registration failed "+ ise);
       }
       AllServiceListener asListen = new AllServiceListener(false);
       try {
         bc.addServiceListener(asListen);
       } catch (IllegalStateException ise) {
-        teststatus  = false;
-        err.println("all service listener registration failed "+ ise
-                    + " :FRAMEsl20A:FAIL");
+        err.println("all service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+        fail("all service listener registration failed "+ ise);
       }
 
       int[] expectedServiceEventTypes = new int[]{
@@ -728,22 +722,6 @@ public class ServiceListenerTestSuite
         fail("Failed to stop bundle, got exception " + e);
       }
 
-      // Stop buSL2
-      try {
-        out.println("Stop buSL2: " +buSL2);
-        buSL2.stop();
-      } catch (BundleException bex) {
-        err.println("Failed to stop bundle, got exception: "
-                    +bex.getNestedException());
-        bex.printStackTrace(err);
-        fail("Failed to stop bundle, got exception: "
-             + bex.getNestedException());
-      } catch (Exception e) {
-        err.println("Failed to stop bundle, got exception " +e);
-        e.printStackTrace(err);
-        fail("Failed to stop bundle, got exception " + e);
-      }
-
 
       // Stop buSL3
       try {
@@ -786,7 +764,7 @@ public class ServiceListenerTestSuite
         fail("Service listener event notification error");
       }
 
-      assertTrue("", sListen.teststatus);
+      assertTrue("Service listener checks", sListen.teststatus);
       try {
         bc.removeServiceListener(sListen);
         sListen.clearEvents();
@@ -794,7 +772,264 @@ public class ServiceListenerTestSuite
         fail("service listener removal failed "+ ise);
       }
 
-      assertTrue(asListen.teststatus);
+      assertTrue("All-service listener checks", asListen.teststatus);
+      try {
+        bc.removeServiceListener(asListen);
+        asListen.clearEvents();
+      } catch (IllegalStateException ise) {
+        fail("all service listener removal failed "+ ise);
+      }
+    }
+  }
+
+
+
+  public final static String [] HELP_FRAMESL25A =  {
+    "Checks that the correct service events",
+    "are sent to a registered service listener.",
+    "This case checks ServiceReference.isAssignableTo(..)",
+    "works when the service provider bundle",
+    "gets the service package via require bundle."
+  };
+
+
+  class FrameSL25a
+    extends FWTestCase
+  {
+    public void runTest() throws Throwable {
+
+      ServiceListener sListen = new ServiceListener(false);
+      try {
+        bc.addServiceListener(sListen);
+      } catch (IllegalStateException ise) {
+        err.println("service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+
+        fail("service listener registration failed "+ ise);
+      }
+      AllServiceListener asListen = new AllServiceListener(false);
+      try {
+        bc.addServiceListener(asListen);
+      } catch (IllegalStateException ise) {
+        err.println("all service listener registration failed "+ ise);
+        ise.printStackTrace(err);
+        fail("all service listener registration failed "+ ise);
+      }
+
+      int[] expectedServiceEventTypes = new int[]{
+        // Startup
+        ServiceEvent.REGISTERED,      // Activator at start of buSL1
+        ServiceEvent.REGISTERED,      // FooService at start of buSL4
+        ServiceEvent.REGISTERED,      // Activator at start of buSL3
+
+        // Stop buSL4
+        ServiceEvent.UNREGISTERING,   // FooService at first stop of buSL4
+
+        // Shutdown
+        ServiceEvent.UNREGISTERING,   // Activator at stop of buSL1
+        ServiceEvent.UNREGISTERING,   // Activator at stop of buSL3
+      };
+
+
+      // Start buSL1 to ensure that the Service package is available.
+      try {
+        out.println("Starting buSL1: " +buSL1);
+        buSL1.start();
+      } catch (BundleException bex) {
+        err.println("Failed to start bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to start bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to start bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to start bundle, got exception " + e);
+      }
+
+
+      // Start buSL4 that will require the serivce package and publish
+      // FooService
+      try {
+        out.println("Starting buSL4: " +buSL4);
+        buSL4.start();
+      } catch (BundleException bex) {
+        err.println("Failed to start bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to start bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to start bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to start bundle, got exception " + e);
+      }
+
+      // Start buSL3 that will require the serivce package and get the service
+      try {
+        out.println("Starting buSL3: " +buSL3);
+        buSL3.start();
+      } catch (BundleException bex) {
+        err.println("Failed to start bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to start bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to start bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to start bundle, got exception " + e);
+      }
+
+      // sleep to stabelize state.
+      try {
+        Thread.sleep(300);
+      } catch (Exception e) {
+        err.println("Unexpected excpetion during sleep:" +e);
+        e.printStackTrace(err);
+      }
+
+      // Check that buSL3 has been notified about the FooService.
+      out.println("Check that FooService is added to service tracker in buSL3");
+      ServiceReference buSL3SR
+        = bc.getServiceReference("org.knopflerfish.bundle.foo.Activator3");
+      assertNotNull("No activator service reference.", buSL3SR);
+      Object buSL3Activator = bc.getService(buSL3SR);
+      assertNotNull("No activator service.", buSL3Activator);
+      Field serviceAddedField3
+        = buSL3Activator.getClass().getField("serviceAdded");
+      assertTrue("bundleSL3 not notified about presence FooService",
+                 serviceAddedField3.getBoolean(buSL3Activator));
+      out.println("buSL3Activator.serviceAdded is true");
+      bc.ungetService(buSL3SR);
+      buSL3Activator = null;
+      buSL3SR = null;
+
+      // Check that buSL1 has been notified about the FooService.
+      out.println("Check that FooService is added to service tracker in buSL1");
+      ServiceReference buSL1SR
+        = bc.getServiceReference("org.knopflerfish.bundle.foo.Activator");
+      assertNotNull("No activator service reference.", buSL1SR);
+      Object buSL1Activator = bc.getService(buSL1SR);
+      assertNotNull("No activator service.", buSL1Activator);
+      Field serviceAddedField
+        = buSL1Activator.getClass().getField("serviceAdded");
+      assertTrue("bundleSL1 not notified about presence FooService",
+                 serviceAddedField.getBoolean(buSL1Activator));
+      out.println("buSL1Activator.serviceAdded is true");
+      bc.ungetService(buSL1SR);
+      buSL1Activator = null;
+      buSL1SR = null;
+
+      // Stop the service provider: buSL4
+      try {
+        out.println("Stop buSL4: " +buSL4);
+        buSL4.stop();
+      } catch (BundleException bex) {
+        err.println("Failed to stop bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to stop bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to stop bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to stop bundle, got exception " + e);
+      }
+
+      // sleep to stabelize state.
+      try {
+        Thread.sleep(300);
+      } catch (Exception e) {
+        err.println("Unexpected excpetion during sleep:" +e);
+        e.printStackTrace(err);
+      }
+
+
+      // Check that buSL3 has been notified about the removal of FooService.
+      out.println("Check that FooService is removed from service "
+                  +"tracker in buSL3");
+      buSL3SR
+        = bc.getServiceReference("org.knopflerfish.bundle.foo.Activator3");
+      assertNotNull("No activator service reference.", buSL3SR);
+      buSL3Activator = bc.getService(buSL3SR);
+      assertNotNull("No activator service.", buSL3Activator);
+      Field serviceRemovedField3
+        = buSL3Activator.getClass().getField("serviceRemoved");
+      assertTrue("bundleSL3 not notified about removal of FooService",
+                 serviceRemovedField3.getBoolean(buSL3Activator));
+      out.println("buSL3Activator.serviceRemoved is true");
+      bc.ungetService(buSL3SR);
+      buSL3Activator = null;
+      buSL3SR = null;
+
+
+      // Stop buSL1
+      try {
+        out.println("Stop buSL1: " +buSL1);
+        buSL1.stop();
+      } catch (BundleException bex) {
+        err.println("Failed to stop bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to stop bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to stop bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to stop bundle, got exception " + e);
+      }
+
+      // Stop buSL3
+      try {
+        out.println("Stop buSL3: " +buSL3);
+        buSL3.stop();
+      } catch (BundleException bex) {
+        err.println("Failed to stop bundle, got exception: "
+                    +bex.getNestedException());
+        bex.printStackTrace(err);
+        fail("Failed to stop bundle, got exception: "
+             + bex.getNestedException());
+      } catch (Exception e) {
+        err.println("Failed to stop bundle, got exception " +e);
+        e.printStackTrace(err);
+        fail("Failed to stop bundle, got exception " + e);
+      }
+
+
+      // sleep to stabelize state.
+      try {
+        Thread.sleep(300);
+      } catch (Exception e) {
+        err.println("Unexpected excpetion during sleep:" +e);
+        e.printStackTrace(err);
+      }
+
+      // Check service events seen by this class (no connection to the
+      // service package so we should see all events for the FooService)
+      out.println("Checking ServiceEvents(ServiceListener):");
+      if (!sListen.checkEvents(expectedServiceEventTypes)) {
+        err.println("Service listener event notification error"
+                    +":FRAMEsl25A:FAIL");
+        fail("Service listener event notification error");
+      }
+
+      out.println("Checking ServiceEvents(AllServiceListener):");
+      if (!asListen.checkEvents(expectedServiceEventTypes)) {
+        err.println("All service listener event notification error :"
+                    +":FRAMEsl25A:FAIL");
+        fail("Service listener event notification error");
+      }
+
+      assertTrue("Service listener checks", sListen.teststatus);
+      try {
+        bc.removeServiceListener(sListen);
+        sListen.clearEvents();
+      } catch (IllegalStateException ise) {
+        fail("service listener removal failed "+ ise);
+      }
+
+      assertTrue("All-service listener checks", asListen.teststatus);
       try {
         bc.removeServiceListener(asListen);
         asListen.clearEvents();
