9e497e312bad82ba46cbcd3784a9aad972bde707
jan <jan@87908148-3fd6-0310-a573-b04dead93e22>
2010-03-22 09:54:08 +0000

    Don't complain so much (DEBUG instead of INFO) if bundle file version qualifier doesn't exist and bundle manifest has one. Send status mails from nightly builds.

--- knopflerfish.org/branches/kf3/tools/nightlybuild/build.sh
+++ knopflerfish.org/branches/kf3/tools/nightlybuild/build.sh
@@ -4,7 +4,9 @@ BUILD_HOME=`dirname $0`
 BUILD_HOME=`cd ${BUILD_HOME};/bin/pwd`
 cd ${BUILD_HOME}
 
-TAGS="trunk"
+TAGS="branches/kf3 trunk"
+
+STATUS_MAIL=devel@makewave.com
 
 #
 # Clean, update and build for the given TAG.
@@ -31,7 +33,11 @@ build() {
 
     echo "= `date +"%C%y-%m-%d %H:%M:%S"` Build ================================="
     # Start build (separate command to ensure that everything can be updated).
-    make -f Makefile_${TAGs} TAG=${TAG} all
+    if make -f Makefile_${TAGs} TAG=${TAG} all ; then
+        tail ${LOG_FILE_NAME} | grep Total | mail -s "KF ${TAG} build successful" ${STATUS_MAIL}
+    else
+        tail ${LOG_FILE_NAME} | mail -s "KF ${TAG} build FAILED" ${STATUS_MAIL}
+    fi
 }
 
 # Build for all tags listed in TAGS
