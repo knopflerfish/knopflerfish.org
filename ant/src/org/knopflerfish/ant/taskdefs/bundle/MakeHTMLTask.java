d8152be7a34538eeaffd26cc57bc22a114255a90
ekolin <ekolin@makewave.com>
2011-02-08 18:19:17 +0000

    fix out of date javadoc comment.

--- ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
+++ ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
@@ -524,9 +524,9 @@ public class MakeHTMLTask
    * <code>${jardir.name}/&ast;&ast;/&ast;.jar</code>.
    * </ul>
    *
-   * @return bundle archives object holding the bundle archives related to this
-   *         project or null if the project has not defined the property
-   *         "jars.dir".
+   * @return bundle archives object holding the bundle archives seleted by the
+   *         file sets described above or null if the project has not defined
+   *         the property "jars.dir".
    */
   private BundleArchives getBundleArchives() {
     final List fileSets = new ArrayList();
