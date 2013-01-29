df2f38675ba1e4f91f51ae56c5f7b397d191feb8
perg <perg@87908148-3fd6-0310-a573-b04dead93e22>
2013-01-29 20:52:38 +0000

    updated to R4.3

--- knopflerfish.org/branches/kf_osgi43/osgi/bundles/upnp/src/org/osgi/service/upnp/UPnPIcon.java
+++ knopflerfish.org/branches/kf_osgi43/osgi/bundles/upnp/src/org/osgi/service/upnp/UPnPIcon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2002, 2008). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2002, 2010). All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,14 +23,14 @@ import java.io.InputStream;
  * 
  * Each UPnP device can contain zero or more icons.
  * 
- * @version $Revision: 5673 $
+ * @version $Id: 99ba43d7968a3e7b20e003ae8368ecea24ce172d $
  */
 public interface UPnPIcon {
 	/**
 	 * Returns the MIME type of the icon.
 	 * 
 	 * This method returns the format in which the icon graphics, read from the
-	 * <code>InputStream</code> object obtained by the <code>getInputStream()</code>
+	 * {@code InputStream} object obtained by the {@code getInputStream()}
 	 * method, is encoded.
 	 * <p>
 	 * The format of the returned string is in accordance to RFC2046. A list of
@@ -38,6 +38,10 @@ public interface UPnPIcon {
 	 * href="http://www.iana.org/assignments/media-types/">IANA</a>.
 	 * <p>
 	 * Typical values returned include: "image/jpeg" or "image/gif"
+	 * 
+	 * <p>
+	 * This method must continue to return the icon MIME type after the UPnP
+	 * device has been removed from the network.
 	 * 
 	 * @return The MIME type of the encoded icon.
 	 */
@@ -48,6 +52,10 @@ public interface UPnPIcon {
 	 * 
 	 * If the actual width of the icon is unknown, -1 is returned.
 	 * 
+	 * <p>
+	 * This method must continue to return the icon width after the UPnP device
+	 * has been removed from the network.
+	 * 
 	 * @return The width in pixels, or -1 if unknown.
 	 */
 	int getWidth();
@@ -57,6 +65,10 @@ public interface UPnPIcon {
 	 * 
 	 * If the actual height of the icon is unknown, -1 is returned.
 	 * 
+	 * <p>
+	 * This method must continue to return the icon height after the UPnP device
+	 * has been removed from the network.
+	 * 
 	 * @return The height in pixels, or -1 if unknown.
 	 */
 	int getHeight();
@@ -65,16 +77,23 @@ public interface UPnPIcon {
 	 * Returns the size of the icon in bytes.
 	 * 
 	 * This method returns the number of bytes of the icon available to read
-	 * from the <code>InputStream</code> object obtained by the
-	 * <code>getInputStream()</code> method. If the actual size can not be
+	 * from the {@code InputStream} object obtained by the
+	 * {@code getInputStream()} method. If the actual size can not be
 	 * determined, -1 is returned.
 	 * 
 	 * @return The icon size in bytes, or -1 if the size is unknown.
+	 * 
+	 * @throws IllegalStateException if the UPnP device has been removed from
+	 *         the network.
 	 */
 	int getSize();
 
 	/**
 	 * Returns the color depth of the icon in bits.
+	 * 
+	 * <p>
+	 * This method must continue to return the icon depth after the UPnP device
+	 * has been removed from the network.
 	 * 
 	 * @return The color depth in bits. If the actual color depth of the icon is
 	 *         unknown, -1 is returned.
@@ -82,17 +101,20 @@ public interface UPnPIcon {
 	int getDepth();
 
 	/**
-	 * Returns an <code>InputStream</code> object for the icon data.
+	 * Returns an {@code InputStream} object for the icon data.
 	 * 
-	 * The <code>InputStream</code> object provides a way for a client to read the
+	 * The {@code InputStream} object provides a way for a client to read the
 	 * actual icon graphics data. The number of bytes available from this
-	 * <code>InputStream</code> object can be determined via the
-	 * <code>getSize()</code> method. The format of the data encoded can be
-	 * determined by the MIME type availble via the <code>getMimeType()</code>
-	 * method.
+	 * {@code InputStream} object can be determined via the {@code getSize()}
+	 * method. The format of the data encoded can be determined by the MIME type
+	 * available via the {@code getMimeType()} method.
 	 * 
 	 * @return An InputStream to read the icon graphics data from.
-	 * @throws IOException If the <code>InputStream</code> cannot be returned.
+	 * 
+	 * @throws IOException If the {@code InputStream} cannot be returned.
+	 * @throws IllegalStateException if the UPnP device has been removed from
+	 *         the network.
+	 * 
 	 * @see UPnPIcon#getMimeType()
 	 */
 	InputStream getInputStream() throws IOException;
