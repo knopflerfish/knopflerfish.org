f1e18efee18ba7b6fc7235683aa14015eff4b348
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2013-02-01 14:38:01 +0000

    Version 3.8.1.kf4-001, compile for Minimum EE, added osgi.ee requirement, updated package versions, add typecasts to null.

--- knopflerfish.org/branches/kf_osgi43/osgi/bundles_opt/junit/junit/src/org/knopflerfish/bundle/junit/JUnitServiceImpl.java
+++ knopflerfish.org/branches/kf_osgi43/osgi/bundles_opt/junit/junit/src/org/knopflerfish/bundle/junit/JUnitServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2009, KNOPFLERFISH project
+ * Copyright (c) 2004-2013, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -432,7 +432,7 @@ public class JUnitServiceImpl implements JUnitService {
     try {
       Method m = target.getClass().getMethod(methodName,
                                              new Class[] { });
-      val = (String)m.invoke(target, null);
+      val = (String) m.invoke(target, (Object[]) null);
     } catch (Exception e) {
       return defVal;
     }
