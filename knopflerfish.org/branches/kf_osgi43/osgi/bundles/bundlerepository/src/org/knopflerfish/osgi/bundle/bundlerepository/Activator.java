e28fa732e22d16090befc6bc70f9d4f899f21db1
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2013-02-01 15:41:27 +0000

    Compile for J2SE 1.4 EE, fixed null types.

--- knopflerfish.org/branches/kf_osgi43/osgi/bundles/bundlerepository/src/org/knopflerfish/osgi/bundle/bundlerepository/Activator.java
+++ knopflerfish.org/branches/kf_osgi43/osgi/bundles/bundlerepository/src/org/knopflerfish/osgi/bundle/bundlerepository/Activator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2010, KNOPFLERFISH project
+ * Copyright (c) 2004-2013, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -158,8 +158,8 @@ public class Activator implements BundleActivator, Runnable
           BundleContext.class
         });
       Object obj = cons.newInstance(new Object[] { bc });
-      Method m = clazz.getMethod(methodName, null);
-      m.invoke(obj, null);
+      Method m = clazz.getMethod(methodName, (Class[]) null);
+      m.invoke(obj, (Object[]) null);
 
       //      System.out.println("invoked " + m);
     }  catch (Throwable th) {
