4c91b112052480a2957615aa68633f2ba7426306
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2013-02-11 16:42:44 +0000

    Fix file sufix and contents

---
+++ knopflerfish.org/branches/kf_osgi43/osgi/ee/src/java/lang/Override.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value=ElementType.METHOD)
+@Retention(value=RetentionPolicy.SOURCE)
+public @interface Override
+{
+}
