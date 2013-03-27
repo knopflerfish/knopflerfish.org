1703fad106b1b69e850e311f93dd83af4fb07798
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2013-03-27 16:12:07 +0000

    Use generic types, remove warning.

--- knopflerfish.org/branches/kf_osgi43/osgi/bundles/device/basicdriverlocator/src/org/knopflerfish/bundle/basicdriverlocator/BasicDriverLocator.java
+++ knopflerfish.org/branches/kf_osgi43/osgi/bundles/device/basicdriverlocator/src/org/knopflerfish/bundle/basicdriverlocator/BasicDriverLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2008, KNOPFLERFISH project
+ * Copyright (c) 2003-2013, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,9 +44,10 @@ import java.util.Hashtable;
 import java.util.Properties;
 import java.util.Vector;
 
-import org.knopflerfish.service.log.LogRef;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.device.DriverLocator;
+
+import org.knopflerfish.service.log.LogRef;
 
 /**
  * Basic implementation of a DriverLocator.
@@ -69,236 +70,252 @@ import org.osgi.service.device.DriverLocator;
  *
  *
  */
-public class BasicDriverLocator implements DriverLocator {
+public class BasicDriverLocator
+  implements DriverLocator
+{
 
-    /** Context service is running in */
-    BundleContext bc = null;
+  /** Context service is running in */
+  BundleContext bc = null;
 
-    String dbResourceName = "/data/driverDB.props";
+  String dbResourceName = "/data/driverDB.props";
 
-    // Utility class for logging
-    LogRef log;
+  // Utility class for logging
+  LogRef log;
 
-    String jarbase = "";
+  String jarbase = "";
 
-    BasicDriverLocator(BundleContext bc) {
-        this.bc = bc;
+  BasicDriverLocator(BundleContext bc)
+  {
+    this.bc = bc;
 
-        // Try to get log service and handle it's life cycle
-        log = new LogRef(bc);
+    // Try to get log service and handle it's life cycle
+    log = new LogRef(bc);
 
-        String jars = bc.getProperty("org.knopflerfish.gosg.jars");
-        jarbase = (null==jars || jars.length()==0) ? "file:" : jars;
+    final String jars = bc.getProperty("org.knopflerfish.gosg.jars");
+    jarbase = (null == jars || jars.length() == 0) ? "file:" : jars;
+  }
+
+  /**
+   * Implements DriverLocator.
+   *
+   * Loads driver DB, then calls match()
+   */
+  public String[] findDrivers(@SuppressWarnings("rawtypes") Dictionary dict)
+  {
+    final Hashtable<String, DriverInfo> drivers = loadDriverDB();
+
+    return match(drivers, dict);
+  }
+
+  /**
+   * Implements DriverLocator.
+   *
+   * Load a driver bundle using URL info.
+   *
+   * @return Stream to driver bundle, null if no driver is found.
+   */
+  public InputStream loadDriver(String id)
+      throws IOException
+  {
+    final Hashtable<String, DriverInfo> drivers = loadDriverDB();
+
+    final DriverInfo di = drivers.get(id);
+    InputStream stream = null;
+
+    if (di == null) {
+      log.warn("No id" + id);
+      throw new IOException("No driver id '" + id + "'");
     }
+    try {
+      String strURL = di.url;
+      if (strURL.indexOf(":") == -1) {
+        strURL = jarbase + strURL;
+      }
 
-    /**
-     * Implements DriverLocator.
-     *
-     * Loads driver DB, then calls match()
-     */
-    public String[] findDrivers(Dictionary dict) {
-        Hashtable drivers = loadDriverDB();
+      final URL url = new URL(strURL);
 
-        return match(drivers, dict);
+      stream = url.openStream();
+    } catch (final MalformedURLException e) {
+      log.error("bad URL for in " + di, e);
+      throw new IOException("Bad driver URL " + e);
+    } catch (final IOException e) {
+      log.error("can't connect to URL in " + di, e);
+
+      // rethrow exception
+      throw e;
     }
+    return stream;
+  }
 
-    /**
-     * Implements DriverLocator.
-     *
-     * Load a driver bundle using URL info.
-     *
-     * @return Stream to driver bundle, null if no driver is found.
-     */
-    public InputStream loadDriver(String id) throws IOException {
-        Hashtable drivers = loadDriverDB();
+  /**
+   * Load driver database.
+   *
+   * @return Hashtable String (driver id) -> DriverInfo
+   */
+  Hashtable<String, DriverInfo> loadDriverDB()
+  {
+    final Hashtable<String, DriverInfo> d = new Hashtable<String, DriverInfo>();
+    InputStream instream = null;
 
-        DriverInfo di = (DriverInfo) drivers.get(id);
-        InputStream stream = null;
+    try {
+      String dbURL =
+        System.getProperty("org.osgi.service.basicdriverlocator.dburl");
 
-        if (di == null) {
-            log.warn("No id" + id);
-            throw new IOException("No driver id '" + id + "'");
+      // If property exists, try to open URL, otherwise use internal
+      // props.
+      if (dbURL != null && !dbURL.equals("")) {
+        if (dbURL.indexOf(":") == -1) {
+          dbURL = jarbase + dbURL;
         }
+        final URL url = new URL(dbURL);
+        log.info("read external props file: " + dbURL);
+        instream = url.openStream();
+      } else {
+        log.info("read internal props file: " + dbResourceName);
+        instream = getClass().getResourceAsStream(dbResourceName);
+      }
+
+      final Properties db = new Properties();
+
+      db.load(instream);
+
+      final int nCount = Integer.parseInt((String) db.get("count"));
+
+      for (int i = 0; i < nCount; i++) {
         try {
-            String strURL = di.url;
-            if (strURL.indexOf(":") == -1) {
-                strURL = jarbase + strURL;
-            }
-
-            URL url = new URL(strURL);
-
-            stream = url.openStream();
-        } catch (MalformedURLException e) {
-            log.error("bad URL for in " + di, e);
-            throw new IOException("Bad driver URL " + e);
-        } catch (IOException e) {
-            log.error("can't connect to URL in " + di, e);
-
-            // rethrow exception
-            throw e;
+          final DriverInfo di = new DriverInfo(db, i);
+          d.put(di.id, di);
+        } catch (final Exception e) {
+          log.error("Failed to initialize driver entry " + i, e);
         }
-        return stream;
+      }
+
+    } catch (final Exception e) {
+      log.error("Can't load driverDB: ", e);
+      e.printStackTrace();
+    } finally {
+      try {
+        if (instream != null) {
+          instream.close();
+        }
+      } catch (final IOException e2) {
+        // silently ignore
+      }
     }
 
-    /**
-     * Load driver database.
-     *
-     * @return Hashtable String (driver id) -> DriverInfo
-     */
-    Hashtable loadDriverDB() {
-        Hashtable d = new Hashtable();
-        InputStream instream = null;
+    return d;
+  }
 
-        try {
-            String dbURL = System
-                    .getProperty("org.osgi.service.basicdriverlocator.dburl");
+  /**
+   * Check all Drivers in drivers for match against props.
+   *
+   * @param drivers
+   *          String (driver ID) -> DriverInfo
+   * @param props
+   *          Device properties to match
+   * @return String array of driver IDs
+   */
+  String[] match(Hashtable<String, DriverInfo> drivers, @SuppressWarnings("rawtypes") Dictionary dict)
+  {
+    final Vector<String> r = new Vector<String>();
 
-            // If property exists, try to open URL, otherwise use internal
-            // props.
-            if (dbURL != null && !dbURL.equals("")) {
-                if (dbURL.indexOf(":") == -1) {
-                    dbURL = jarbase + dbURL;
-                }
-                URL url = new URL(dbURL);
-                log.info("read external props file: " + dbURL);
-                instream = url.openStream();
-            } else {
-                log.info("read internal props file: " + dbResourceName);
-                instream = getClass().getResourceAsStream(dbResourceName);
-            }
+    for (final Enumeration<DriverInfo> e = drivers.elements(); e.hasMoreElements();) {
+      final DriverInfo di = e.nextElement();
 
-            Properties db = new Properties();
-
-            db.load(instream);
-
-            int nCount = Integer.parseInt((String) db.get("count"));
-
-            for (int i = 0; i < nCount; i++) {
-                try {
-                    DriverInfo di = new DriverInfo(db, i);
-                    d.put(di.id, di);
-                } catch (Exception e) {
-                    log.error("Failed to initialize driver entry " + i, e);
-                }
-            }
-
-        } catch (Exception e) {
-            log.error("Can't load driverDB: ", e);
-            e.printStackTrace();
-        } finally {
-            try {
-                if (instream != null)
-                    instream.close();
-            } catch (IOException e2) {
-                // silently ignore
-            }
-        }
-
-        return d;
+      if (di.match(dict)) {
+        r.addElement(di.id);
+      }
     }
 
-    /**
-     * Check all Drivers in drivers for match agains props.
-     *
-     * @param drivers
-     *            String (driver ID) -> DriverInfo
-     * @param props
-     *            Device properties to match
-     * @return String array od driver IDs
-     */
-    String[] match(Hashtable drivers, Dictionary dict) {
-        Vector r = new Vector();
+    // Create string array, since API says so
+    final String[] rl = new String[r.size()];
 
-        for (Enumeration e = drivers.elements(); e.hasMoreElements();) {
-            DriverInfo di = (DriverInfo) e.nextElement();
+    r.copyInto(rl);
 
-            if (di.match(dict)) {
-                r.addElement(di.id);
-            }
-        }
-
-        // Create string array, since API says so
-        String[] rl = new String[r.size()];
-
-        r.copyInto(rl);
-
-        return rl;
-    }
+    return rl;
+  }
 }
 
 /**
  * Utility class for holding driver database
  */
-class DriverInfo {
-    String url;
+class DriverInfo
+{
+  String url;
 
-    String id;
+  String id;
 
-    String category;
+  String category;
 
-    String vendor;
+  String vendor;
 
-    String product;
+  String product;
 
-    String desc;
+  String desc;
 
-    /**
-     * Create from Properties.
-     *
-     * Values of the form <code>number</code>.key are get() from props to
-     * build the DriverInfo.
-     *
-     * @param p
-     *            Properties containing driver info
-     * @param number
-     *            count number inte named properties
-     */
-    DriverInfo(Properties p, int number) {
-        id = (String) p.get("" + number + ".id");
-        url = (String) p.get("" + number + ".url");
-        desc = (String) p.get("" + number + ".desc");
-        vendor = (String) p.get("" + number + ".vendor");
-        product = (String) p.get("" + number + ".product");
-        category = (String) p.get("" + number + ".category");
-    }
+  /**
+   * Create from Properties.
+   *
+   * Values of the form <code>number</code>.key are get() from props to build
+   * the DriverInfo.
+   *
+   * @param p
+   *          Properties containing driver info
+   * @param number
+   *          count number inte named properties
+   */
+  DriverInfo(Properties p, int number)
+  {
+    id = (String) p.get("" + number + ".id");
+    url = (String) p.get("" + number + ".url");
+    desc = (String) p.get("" + number + ".desc");
+    vendor = (String) p.get("" + number + ".vendor");
+    product = (String) p.get("" + number + ".product");
+    category = (String) p.get("" + number + ".category");
+  }
 
-    /**
-     * Matching method used by locator.
-     *
-     * @return true if DriverInfo matches props, false otherwise
-     */
-    boolean match(Dictionary props) {
-        boolean b = cmp(category, (String) props.get("category"))
-                && cmp(product, (String) props.get("product"))
-                && cmp(vendor, (String) props.get("vendor"));
+  /**
+   * Matching method used by locator.
+   *
+   * @return true if DriverInfo matches props, false otherwise
+   */
+  boolean match(@SuppressWarnings("rawtypes") Dictionary props)
+  {
+    final boolean b =
+      cmp(category, (String) props.get("category"))
+          && cmp(product, (String) props.get("product"))
+          && cmp(vendor, (String) props.get("vendor"));
 
-        return b;
-    }
+    return b;
+  }
 
-    /**
-     * Compare method which allows empty strings and simple wildcards.
-     *
-     * null, "" and "*" are match-all wildcards.
-     *
-     * @param s1
-     *            string to compare to s2
-     * @param s2
-     *            string to compare to s1
-     * @return true id s1 compares nicely to s2
-     */
-    boolean cmp(String s1, String s2) {
-        return s1 == null || s2 == null || s1.equals("") || s2.equals("")
-                || s1.equals("*") || s2.equals("*") || s1.equalsIgnoreCase(s2);
-    }
+  /**
+   * Compare method which allows empty strings and simple wildcards.
+   *
+   * null, "" and "*" are match-all wildcards.
+   *
+   * @param s1
+   *          string to compare to s2
+   * @param s2
+   *          string to compare to s1
+   * @return true if s1 compares nicely to s2
+   */
+  boolean cmp(String s1, String s2)
+  {
+    return s1 == null || s2 == null || s1.equals("") || s2.equals("")
+           || s1.equals("*") || s2.equals("*") || s1.equalsIgnoreCase(s2);
+  }
 
-    /**
-     * Convert to string.
-     *
-     * @return Human-readable string representation
-     */
-    public String toString() {
-        return "id=" + id + ", url=" + url + ", desc=" + desc + ", product="
-                + product + ", vendor=" + vendor;
-    }
+  /**
+   * Convert to string.
+   *
+   * @return Human-readable string representation
+   */
+  @Override
+  public String toString()
+  {
+    return "id=" + id + ", url=" + url + ", desc=" + desc + ", product="
+           + product + ", vendor=" + vendor;
+  }
 
 }
--- knopflerfish.org/branches/kf_osgi43/osgi/bundles/device/device/src/org/knopflerfish/bundle/device/MatchImpl.java
+++ knopflerfish.org/branches/kf_osgi43/osgi/bundles/device/device/src/org/knopflerfish/bundle/device/MatchImpl.java
@@ -43,136 +43,158 @@ import org.osgi.service.device.Device;
 import org.osgi.service.device.DriverLocator;
 import org.osgi.service.device.Match;
 
-public class MatchImpl implements Match {
-    static final int UNKNOWN = -1;
+public class MatchImpl
+  implements Match
+{
+  static final int UNKNOWN = -1;
 
-    private Activator act;
+  private final Activator act;
 
-    private ServiceReference dev;
+  private final ServiceReference<?> dev;
 
-    private String id;
+  private final String id;
 
-    private DriverRef ref;
+  private DriverRef ref;
 
-    private int match;
+  private int match;
 
-    private Vector /* DriverLocator */dls;
+  private Vector<DriverLocator> dls;
 
-    private Bundle b;
+  private Bundle b;
 
-    MatchImpl(Activator act, ServiceReference dev, String id) {
-        this.act = act;
-        this.dev = dev;
-        this.id = id;
-        match = act.getCachedMatch(id, dev);
+  MatchImpl(Activator act, ServiceReference<?> dev, String id)
+  {
+    this.act = act;
+    this.dev = dev;
+    this.id = id;
+    match = act.getCachedMatch(id, dev);
+  }
+
+  MatchImpl(Activator act, ServiceReference<?> dev, DriverRef dr)
+  {
+    this.act = act;
+    this.dev = dev;
+    id = dr.id;
+    ref = dr;
+    match = act.getCachedMatch(id, dev);
+  }
+
+  boolean equals(String id)
+  {
+    return this.id.equals(id);
+  }
+
+  boolean connect(DriverRef dr)
+  {
+    if (id.equals(dr.id) && (ref == dr || ref == null)) {
+      ref = dr;
+      return true;
     }
+    return false;
+  }
 
-    MatchImpl(Activator act, ServiceReference dev, DriverRef dr) {
-        this.act = act;
-        this.dev = dev;
-        id = dr.id;
-        ref = dr;
-        match = act.getCachedMatch(id, dev);
+  int compare(MatchImpl m)
+  {
+    int cmp = getMatchValue() - m.getMatchValue();
+    if (cmp == 0) {
+      cmp = ref.ranking - m.ref.ranking;
     }
-
-    boolean equals(String id) {
-        return this.id.equals(id);
+    if (cmp == 0) {
+      cmp = ref.servid < m.ref.servid ? 1 : -1;
     }
+    return cmp;
+  }
 
-    boolean connect(DriverRef dr) {
-        if (id.equals(dr.id) && (ref == dr || ref == null)) {
-            ref = dr;
-            return true;
+  void addDriverLocator(DriverLocator dl)
+  {
+    if (dls == null) {
+      dls = new Vector<DriverLocator>();
+    }
+    dls.addElement(dl);
+  }
+
+  Bundle getBundle()
+  {
+    return b;
+  }
+
+  private boolean load()
+  {
+    final String name = Activator.DYNAMIC_DRIVER_TAG + id;
+    final Bundle[] ba = act.bc.getBundles();
+    if (ba != null) {
+      for (final Bundle element : ba) {
+        if (name.equals(element.getLocation())) {
+          return false;
         }
-        return false;
+      }
     }
-
-    int compare(MatchImpl m) {
-        int cmp = getMatchValue() - m.getMatchValue();
-        if (cmp == 0)
-            cmp = ref.ranking - m.ref.ranking;
-        if (cmp == 0)
-            cmp = ref.servid < m.ref.servid ? 1 : -1;
-        return cmp;
-    }
-
-    void addDriverLocator(DriverLocator dl) {
-        if (dls == null)
-            dls = new Vector();
-        dls.addElement(dl);
-    }
-
-    Bundle getBundle() {
-        return b;
-    }
-
-    private boolean load() {
-        String name = Activator.DYNAMIC_DRIVER_TAG + id;
-        Bundle[] ba = act.bc.getBundles();
-        if (ba != null) {
-            for (int i = 0; i < ba.length; i++) {
-                if (name.equals(ba[i].getLocation())) {
-                    return false;
-                }
-            }
+    if (dls != null) {
+      for (int i = 0; i < dls.size(); i++) {
+        if (load1(name, dls.elementAt(i))) {
+          return true;
         }
-        if (dls != null) {
-            for (int i = 0; i < dls.size(); i++) {
-                if (load1(name, (DriverLocator) dls.elementAt(i)))
-                    return true;
-            }
-        } else {
-            DriverLocator[] locs = act.locators;
-            for (int i = 0; i < locs.length; i++) {
-                if (load1(name, locs[i]))
-                    return true;
-            }
+      }
+    } else {
+      final DriverLocator[] locs =
+        act.locators.toArray(new DriverLocator[act.locators.size()]);
+      for (final DriverLocator loc : locs) {
+        if (load1(name, loc)) {
+          return true;
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    private boolean load1(String name, DriverLocator dl) {
-        InputStream is = null;
+  private boolean load1(String name, DriverLocator dl)
+  {
+    InputStream is = null;
+    try {
+      is = dl.loadDriver(id);
+      b = act.installBundle(name, is);
+      if (b != null) {
+        return true;
+      }
+    } catch (final Exception e) {
+    } finally {
+      try {
+        is.close();
+      } catch (final Exception e1) {
+      }
+    }
+    return false;
+  }
+
+  String attach()
+      throws Exception
+  {
+    if (match > Device.MATCH_NONE && ref == null && b == null && load()) {
+      return null;
+    }
+    match = Device.MATCH_NONE;
+    return ref.drv.attach(dev);
+  }
+
+  public ServiceReference<?> getDriver()
+  {
+    return ref.sr;
+  }
+
+  public int getMatchValue()
+  {
+    if (match == UNKNOWN) {
+      if (ref != null || b != null) {
         try {
-            is = dl.loadDriver(id);
-            b = act.installBundle(name, is);
-            if (b != null)
-                return true;
-        } catch (Exception e) {
-        } finally {
-            try {
-                is.close();
-            } catch (Exception e1) {
-            }
+          match = ref.drv.match(dev);
+          act.putCachedMatch(id, dev, match);
+        } catch (final Exception e) {
+          match = Device.MATCH_NONE;
         }
-        return false;
-    }
-
-    String attach() throws Exception {
-        if (match > Device.MATCH_NONE && ref == null && b == null && load()) {
-            return null;
-        }
+      } else if (!load()) {
         match = Device.MATCH_NONE;
-        return ref.drv.attach(dev);
+      }
     }
-
-    public ServiceReference getDriver() {
-        return ref.sr;
-    }
-
-    public int getMatchValue() {
-        if (match == UNKNOWN) {
-            if (ref != null || b != null) {
-                try {
-                    match = ref.drv.match(dev);
-                    act.putCachedMatch(id, dev, match);
-                } catch (Exception e) {
-                    match = Device.MATCH_NONE;
-                }
-            } else if (!load()) {
-                match = Device.MATCH_NONE;
-            }
-        }
-        return match;
-    }
+    return match;
+  }
 }
