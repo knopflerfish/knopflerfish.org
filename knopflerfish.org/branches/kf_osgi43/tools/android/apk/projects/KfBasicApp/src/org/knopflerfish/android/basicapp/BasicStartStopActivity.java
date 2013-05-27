66e376e0f7d8575c4cd9da0b7122e733208c500b
joek <joek@87908148-3fd6-0310-a573-b04dead93e22>
2013-05-27 08:41:41 +0000

    service<->app messenger added

--- knopflerfish.org/branches/kf_osgi43/tools/android/apk/projects/KfBasicApp/src/org/knopflerfish/android/basicapp/BasicStartStopActivity.java
+++ knopflerfish.org/branches/kf_osgi43/tools/android/apk/projects/KfBasicApp/src/org/knopflerfish/android/basicapp/BasicStartStopActivity.java
@@ -3,34 +3,62 @@ package org.knopflerfish.android.basicapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.knopflerfish.android.service.Knopflerfish;
+import org.knopflerfish.android.service.Knopflerfish.Msg;
 
 public class BasicStartStopActivity extends Activity {
+
+  private Messenger messenger;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // messenger for communication Knopflerfish -> Activity
+    messenger = new Messenger(new Handler() {
+      public void handleMessage(Message m) {
+        Bundle reply = m.getData();
+        Msg msg = (Msg) reply.getSerializable(Knopflerfish.MSG);
+        String s = "";
+        switch (msg) {
+        case STARTING: s = "Framework is starting"; break;
+        case NOP : s = "Framework is already running"; break;
+        case STARTED:
+          s = "Framework was started"; break;
+        case NOT_STARTED: s = "Framework did not start!"; break;
+        case STOPPED: s = "Framework was stopped"; break;
+        case NOT_STOPPED: s = "Framework did not stop!"; break;
+        }
+        showToast(s);
+      }
+    });
+    
     setContentView(R.layout.activity_main);
   }
 
-  public void startKfInit(View view) {
-    // start Knopflerfish service with init action
-    Intent intent = new Intent(this, Knopflerfish.class);
-    intent.setAction(Knopflerfish.ACTION_INIT);
-    startService(intent);
-    
-    updateLinks(true);
-  }
-	
   public void startKf(View view) {
     // start Knopflerfish service
     Intent intent = new Intent(this, Knopflerfish.class);
-    startService(intent);	  
+    intent.putExtra("messenger", messenger);
+    startService(intent);   
 
+    updateLinks(true);
+  }
+  
+  public void startKfInit(View view) {
+    // start Knopflerfish service with init action
+    Intent intent = new Intent(this, Knopflerfish.class);
+    intent.putExtra("messenger", messenger);
+    intent.setAction(Knopflerfish.ACTION_INIT);
+    startService(intent);
+    
     updateLinks(true);
   }
 	
@@ -58,5 +86,4 @@ public class BasicStartStopActivity extends Activity {
     tw.setText(link2);    
   }
 
-  
 }
