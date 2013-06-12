c7bc989795c87676c3d21e6b4f3dad27596767ae
joek <joek@87908148-3fd6-0310-a573-b04dead93e22>
2013-06-12 12:31:58 +0000

    changed to match Knopflerfish service

--- knopflerfish.org/branches/kf_osgi43/tools/android/apk/projects/KfBasicApp/src/org/knopflerfish/android/basicapp/BasicStartStopActivity.java
+++ knopflerfish.org/branches/kf_osgi43/tools/android/apk/projects/KfBasicApp/src/org/knopflerfish/android/basicapp/BasicStartStopActivity.java
@@ -1,52 +1,73 @@
 package org.knopflerfish.android.basicapp;
 
+import org.knopflerfish.android.service.Knopflerfish;
+import org.knopflerfish.android.service.Knopflerfish.Msg;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Messenger;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.knopflerfish.android.service.Knopflerfish;
-import org.knopflerfish.android.service.Knopflerfish.Msg;
-
 public class BasicStartStopActivity extends Activity {
 
-  private Messenger messenger;
+  private BroadcastReceiver receiver;
+  private boolean registered;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // messenger for communication Knopflerfish -> Activity
-    messenger = new Messenger(new Handler() {
-      public void handleMessage(Message m) {
-        Bundle reply = m.getData();
-        Msg msg = (Msg) reply.getSerializable(Knopflerfish.MSG);
-        String s = "";
-        switch (msg) {
-        case STARTING: s = "Framework is starting"; break;
-        case NOP : s = "Framework is already running"; break;
-        case STARTED:
-          s = "Framework was started"; break;
-        case NOT_STARTED: s = "Framework did not start!"; break;
-        case STOPPED: s = "Framework was stopped"; break;
-        case NOT_STOPPED: s = "Framework did not stop!"; break;
+    // broadcast receiver that gets messages from Knopflerfish service
+    if (receiver == null) {
+      receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+          Bundle extras = intent.getExtras();
+          Msg msg = (Msg) extras.getSerializable(Knopflerfish.MSG);
+          String s = null;
+          switch (msg) {
+          case STARTING: s = "Framework is starting"; break;
+          case STARTED: s = "Framework was started"; break;
+          case NOP: s = "Framework is already started"; break;
+          case NOT_STARTED: s = "Framework did not start!"; break;
+          case STOPPED: s = "Framework was stopped"; break;
+          case NOT_STOPPED: s = "Framework did not stop!"; break;
+          }
+          showToast(s);
         }
-        showToast(s);
-      }
-    });
-    
+      };
+    }
+
     setContentView(R.layout.activity_main);
   }
 
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (!registered) {
+      registerReceiver(receiver, new IntentFilter(Knopflerfish.class.getName()));
+      registered = true;
+    }
+  }
+  
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (registered) {
+      unregisterReceiver(receiver);
+      registered = false;
+    }
+  }
+  
   public void startKf(View view) {
     // start Knopflerfish service
     Intent intent = new Intent(this, Knopflerfish.class);
-    intent.putExtra("messenger", messenger);
     startService(intent);   
 
     updateLinks(true);
@@ -55,7 +76,6 @@ public class BasicStartStopActivity extends Activity {
   public void startKfInit(View view) {
     // start Knopflerfish service with init action
     Intent intent = new Intent(this, Knopflerfish.class);
-    intent.putExtra("messenger", messenger);
     intent.setAction(Knopflerfish.ACTION_INIT);
     startService(intent);
     
