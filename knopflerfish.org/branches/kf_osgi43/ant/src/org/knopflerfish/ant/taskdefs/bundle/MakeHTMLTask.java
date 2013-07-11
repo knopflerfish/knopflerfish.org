6b9a2185b87b5723942faa0a13e04c8e4597ae45
ekolin <ekolin@87908148-3fd6-0310-a573-b04dead93e22>
2013-07-11 08:26:49 +0000

    Added expansions for VERSION_PREV, KF_VERSION and RELEASE_DATE to MakeHTMLTask

--- knopflerfish.org/branches/kf_osgi43/ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
+++ knopflerfish.org/branches/kf_osgi43/ant/src/org/knopflerfish/ant/taskdefs/bundle/MakeHTMLTask.java
@@ -377,6 +377,10 @@ public class MakeHTMLTask
                              System.getProperty("user.name"));
       content = Util.replace(content, "$(VERSION)",
                              proj.getProperty("version"));
+      content = Util.replace(content, "$(VERSION_PREV)",
+                             proj.getProperty("version.previous"));
+      content = Util.replace(content, "$(KF_VERSION)",
+                             proj.getProperty("kf_version"));
       content = Util.replace(content, "$(BASE_VERSION)",
                              proj.getProperty("base_version"));
       content = Util.replace(content, "$(BASE_URL)",
@@ -387,6 +391,8 @@ public class MakeHTMLTask
                              proj.getProperty("distrib.name"));
       content = Util.replace(content, "$(RELEASE_NAME)",
                              proj.getProperty("release.name"));
+      content = Util.replace(content, "$(RELEASE_DATE)",
+                             proj.getProperty("release.date"));
       content = Util.replace(content, "$(MESSAGE)",
                              proj.getProperty("release"));
       content = Util.replace(content, "$(BUNDLE_LIST)", bundleList);
