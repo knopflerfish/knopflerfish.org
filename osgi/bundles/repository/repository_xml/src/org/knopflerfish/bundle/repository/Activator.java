2f7ca0e87b25aba7cd3868ac34f81522677d9c2d
perg <perg@makewave.com>
2013-10-07 14:05:42 +0000

    Added Factory service

--- osgi/bundles/repository/repository_xml/src/org/knopflerfish/bundle/repository/Activator.java
+++ osgi/bundles/repository/repository_xml/src/org/knopflerfish/bundle/repository/Activator.java
@@ -1,16 +1,24 @@
 package org.knopflerfish.bundle.repository;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.knopflerfish.service.repository.XmlBackedRepositoryFactory;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 
 public class Activator implements BundleActivator {
+  final static String REPOSITORY_XML_PID = "org.knopflerfish.repository.xml.MSF";
   final static String REPOSITORY_XML_URL = "org.knopflerfish.repository.xml.url";
   BundleContext bc;
   FactoryImpl factory;
   ServiceRegistration<XmlBackedRepositoryFactory> sr;
+  ServiceRegistration<ManagedServiceFactory> msfr;
   
   @Override
   public void start(BundleContext bc) throws Exception {
@@ -19,20 +27,52 @@ public class Activator implements BundleActivator {
     
     String url = bc.getProperty(REPOSITORY_XML_URL);
     if(url != null && !"".equals(url)) {
-      factory.create(url);
+      factory.create(url, null);
     }
     
     sr = bc.registerService(XmlBackedRepositoryFactory.class, factory, null);
+    
+    
+    Hashtable<String, String> h = new Hashtable<String, String>();
+    h.put(Constants.SERVICE_PID, REPOSITORY_XML_PID);
+    msfr = bc.registerService(ManagedServiceFactory.class, new MSF(), h);
   }
 
 
 
   @Override
   public void stop(BundleContext bc) throws Exception {
+    if(msfr != null) {
+      msfr.unregister();
+    }
     if(sr != null) {
       sr.unregister();
     }
     factory.destroyAll();
   }
+  
+  class MSF implements ManagedServiceFactory {
 
+    @Override
+    public String getName() {
+      return "Xml-backed Repository Factory";
+    }
+
+    @Override
+    public synchronized void updated(String pid, Dictionary<String, ?> p)
+        throws ConfigurationException {
+
+        try {
+          factory.create((String)p.get("url"), pid);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }  
+    }
+
+    @Override
+    public synchronized void deleted(String pid) {
+      factory.destroy(pid);
+    }
+    
+  }
 }
--- osgi/bundles/repository/repository_xml/src/org/knopflerfish/bundle/repository/FactoryImpl.java
+++ osgi/bundles/repository/repository_xml/src/org/knopflerfish/bundle/repository/FactoryImpl.java
@@ -12,6 +12,7 @@ import org.knopflerfish.bundle.repository.xml.RepositoryXmlParser;
 import org.knopflerfish.service.repository.XmlBackedRepositoryFactory;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.resource.Resource;
 import org.osgi.service.repository.Repository;
@@ -25,20 +26,25 @@ public class FactoryImpl implements XmlBackedRepositoryFactory {
   }
   
   @Override
-  public Object create(String url) throws Exception {
-    if(url != null && !"".equals(url)) {
+  public Object create(String url, Object handle) throws Exception {
+    if(url != null && !"".equals(url) && !repositoryRegistrations.containsKey(url)) {
       InputStream is = new URL(url).openStream();
       Collection<Resource> rs = RepositoryXmlParser.parse(is);
       RepositoryXmlParser.debug(rs);
       if(!rs.isEmpty()) {
         RepositoryImpl repo = new RepositoryImpl(bc, rs);
         Hashtable<String, String> h = new Hashtable<String, String>();
-        h.put("service.pid", "org.knopflerfish.repository.xml");
+        h.put(Constants.SERVICE_PID, "org.knopflerfish.repository.xml");
         //h.put("service.description", "org.knopflerfish.repository.xml");
         //h.put("repository.url", url);
         ServiceRegistration<Repository> sr = bc.registerService(Repository.class, repo, h);
+        
         repositoryRegistrations.put(url, sr);
-        return url;
+        if(handle != null) {
+          // User provided non-url custom handle
+          repositoryRegistrations.put(handle, sr);
+        }
+        return handle == null ? url : handle;
       }
     }
     return null;
@@ -47,6 +53,7 @@ public class FactoryImpl implements XmlBackedRepositoryFactory {
   @Override
   public void destroy(Object handle) {
     ServiceRegistration<Repository> sr = repositoryRegistrations.remove(handle);
+    repositoryRegistrations.values().remove(sr); // Remove url->sr mapping in case user provided custom handle
     if(sr != null) {
       sr.unregister();
     }
