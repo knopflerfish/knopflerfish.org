b8207dcc0ded0994ce880dd0f2669b169304652c
perg <perg@87908148-3fd6-0310-a573-b04dead93e22>
2013-10-07 14:05:42 +0000

    Added Factory service

--- knopflerfish.org/branches/kf_osgi5/osgi/bundles/repository/repository_xml/src/org/knopflerfish/service/repository/XmlBackedRepositoryFactory.java
+++ knopflerfish.org/branches/kf_osgi5/osgi/bundles/repository/repository_xml/src/org/knopflerfish/service/repository/XmlBackedRepositoryFactory.java
@@ -1,6 +1,6 @@
 package org.knopflerfish.service.repository;
 
 public interface XmlBackedRepositoryFactory {
-  public Object create(String url) throws Exception;
+  public Object create(String url, Object handle) throws Exception;
   public void destroy(Object handle) throws Exception;
 }
