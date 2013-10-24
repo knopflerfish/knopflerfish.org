1af2c68385652d18f1e2baa4a63e91f38f84150e
perg <perg@makewave.com>
2013-10-24 12:21:24 +0000

    Fixed parsing of Bundle-License

--- osgi/bundles/repository/repoindex_kf/src/org/knopflerfish/bundle/repository/index/KnopflerfishExtentions.java
+++ osgi/bundles/repository/repoindex_kf/src/org/knopflerfish/bundle/repository/index/KnopflerfishExtentions.java
@@ -2,6 +2,7 @@ package org.knopflerfish.bundle.repository.index;
 
 import java.util.List;
 
+import org.knopflerfish.bundle.repository.index.Util.HeaderEntry;
 import org.osgi.framework.Constants;
 import org.osgi.framework.namespace.IdentityNamespace;
 import org.osgi.service.indexer.Builder;
@@ -15,7 +16,7 @@ public class KnopflerfishExtentions implements ResourceAnalyzer {
   @Override
   public void analyzeResource(Resource resource, List<Capability> capabilities,
       List<Requirement> requirements) throws Exception {
-    System.out.println("CALLED FOR: " + resource.getLocation());
+	System.out.println("REPOINDEX_KF CALLED FOR: " + resource.getLocation());
     if(resource.getManifest() == null) {
       return;
     }
@@ -33,9 +34,11 @@ public class KnopflerfishExtentions implements ResourceAnalyzer {
         { Constants.BUNDLE_DOCURL,
           IdentityNamespace.CAPABILITY_DOCUMENTATION_ATTRIBUTE,
         },
+        /* Special handling
         { "Bundle-License",
           IdentityNamespace.CAPABILITY_LICENSE_ATTRIBUTE
         },
+        */
         { Constants.BUNDLE_CATEGORY,
           "category"
         },
@@ -59,8 +62,35 @@ public class KnopflerfishExtentions implements ResourceAnalyzer {
       String attributeName = m[1];
       b = b.addAttribute(attributeName, manifestHeader);
     }
-    capabilities.add(b.buildCapability());   
+    // Special handling of Bundle-License
+    String manifestHeader = resource.getManifest().getMainAttributes().getValue("Bundle-License");
+    if(manifestHeader != null) {
+    	b = b.addAttribute(IdentityNamespace.CAPABILITY_LICENSE_ATTRIBUTE, parseLicenseHeader(manifestHeader));
+    }
+    
+    capabilities.add(b.buildCapability());
 
   }
+  
+	String parseLicenseHeader(String a) {
+		if (a == null) {
+			return null;
+		} else {
+			StringBuffer sb = new StringBuffer();
+			try {
+				List<HeaderEntry> lic = Util.parseManifestHeader(
+						"Bundle-License", a, true, true, false);
+				for (HeaderEntry he : lic) {
+					if (sb.length() > 0) {
+						sb.append(", ");
+					}
+					sb.append(he.getKey());
+				}
+			} catch (IllegalArgumentException iae) {
+
+			}
+			return sb.toString();
+		}
+	}
 
 }
