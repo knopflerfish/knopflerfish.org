af1f24045524bccf6fa6657fb8f914e5e2d1b3ea
cl <cl@makewave.com>
2016-05-18 11:30:01 +0200

    Correcting travis_upload

--- tools/bintray/bin/travis_upload.sh
+++ tools/bintray/bin/travis_upload.sh
@@ -12,7 +12,7 @@ BINTRAY_URL="https://bintray.com/api/v1"
 
 RELEASE_NOTES_PATH="packages/knopflerfish/kf_r6-snapshot/KnopflerfishBuild/versions/${RELEASE_VERSION}/release_notes"
 
-RELEASE_NOTES_FILE="${DIR}/@DISTRIB_NAME@/release_notes.md
+RELEASE_NOTES_FILE="${DIR}/@DISTRIB_NAME@/release_notes.md"
 
 if [ ! -f $RELEASE_NOTES_FILE ]; then
     echo "Release note file does not exist, exiting: ${RELEASE_NOTES_FILE}"
