71e497277ebf09e2d037029f987943cd639844fd
Jan Stein <jan@makewave.com>
2017-01-23 17:54:03 +0100

    Put ssh opts on cmdline. Drop quiet mode.

--- tools/travis/kf_upload.sh
+++ tools/travis/kf_upload.sh
@@ -11,24 +11,19 @@ RELEASE_DIR=out/distrib_$1
 KF_SERVER=isora.oderland.com
 KF_USER=makewav1
 KF_RELEASES_DIR=public_html-knopflerfish.org/releases/
+SSH_OPT="StrictHostKeyChecking no"
 
 if [ ! -d "$RELEASE_DIR" ] ; then
     echo "Release not found at: $RELEASE_DIR"
     exit 1
 fi
 
-# Allow ssh to KF_SERVER
-# NOTE! You need to clean this if not run on a build server!
-mkdir -p ~/.ssh
-echo "Host $KF_SERVER" >> ~/.ssh/config
-echo "StrictHostKeyChecking no" >> ~/.ssh/config
-
 echo "Uploading KF release $1 to www.knopflerfish.org"
-scp -rpqBi $PRIVATE_KEY $RELEASE_DIR $KF_USER@$KF_SERVER:$KF_RELEASES_DIR/$1
+scp -rpB -o "$SSH_OPT" -i $PRIVATE_KEY $RELEASE_DIR $KF_USER@$KF_SERVER:$KF_RELEASES_DIR/$1
 
 if [ "$2" == "master" ] ; then
     echo "Master build, update release soft-link"
     MAJOR=`echo $1 | cut -d. -f1`
     LINK="current-kf_$MAJOR"
-    ssh -ni $PRIVATE_KEY -l $KF_USER $KF_SERVER "cd $KF_RELEASES_DIR && rm $LINK && ln -s $1 $LINK"
+    ssh -n -o "$SSH_OPT" -i $PRIVATE_KEY -l $KF_USER $KF_SERVER "cd $KF_RELEASES_DIR && rm $LINK && ln -s $1 $LINK"
 fi
