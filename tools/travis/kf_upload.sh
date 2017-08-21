00881270eeb802193cacfe21cf8db716dc35e8f3
Jan Stein <jan@makewave.com>
2017-08-21 23:50:31 +0200

    Merged fixed issue #24.

--- tools/travis/kf_upload.sh
+++ tools/travis/kf_upload.sh
@@ -8,16 +8,23 @@
 #
 PRIVATE_KEY=tools/travis/id_rsa.travis
 RELEASE_DIR=out/distrib_$1
+BUILD_TMP_DIR=out/tmp
 KF_SERVER=isora.oderland.com
 KF_USER=makewav1
 KF_RELEASES_DIR=public_html-knopflerfish.org/releases
-SSH_OPT="StrictHostKeyChecking no"
+SSH_OPT="StrictHostKeyChecking=no"
+
+
+MVN_LOCAL_REMOTE_DIR="$BUILD_TMP_DIR/remote_maven2"
+
+run_gradle() {
+    (cd $BUILD_TMP_DIR; $GRADLE_HOME/bin/gradle updateMavenRepo)
+}
 
 if [ ! -d "$RELEASE_DIR" ] ; then
     echo "Release not found at: $RELEASE_DIR"
     exit 1
 fi
-
 echo "Uploading KF release $1 to www.knopflerfish.org"
 tar czpf - -C $RELEASE_DIR . | ssh -o "$SSH_OPT" -i $PRIVATE_KEY  -l $KF_USER $KF_SERVER "mkdir $KF_RELEASES_DIR/$1 && tar xzpf - -C $KF_RELEASES_DIR/$1"
 
@@ -26,4 +33,13 @@ if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
     MAJOR=`echo $1 | cut -d. -f1`
     LINK="current-kf_$MAJOR"
     ssh -n -o "$SSH_OPT" -i $PRIVATE_KEY -l $KF_USER $KF_SERVER "cd $KF_RELEASES_DIR && rm $LINK && ln -s $1 $LINK"
+    MVN_REMOTE_DIR=/home/makewav1/public_html-resources.knopflerfish.org/repo/maven2-release/
+    # Merge into the main maven repo, fetch, update and push back up
+    mkdir -p $MVN_LOCAL_REMOTE_DIR
+
+    rsync -r -a -v -e "ssh -o $SSH_OPT -i $PRIVATE_KEY" "${KF_USER}@${KF_SERVER}:${MVN_REMOTE_DIR}" $MVN_LOCAL_REMOTE_DIR
+
+    run_gradle
+
+    rsync -r -a -v -i -e "ssh -o $SSH_OPT -i $PRIVATE_KEY" $MVN_LOCAL_REMOTE_DIR/ "${KF_USER}@${KF_SERVER}:${MVN_REMOTE_DIR}"
 fi
