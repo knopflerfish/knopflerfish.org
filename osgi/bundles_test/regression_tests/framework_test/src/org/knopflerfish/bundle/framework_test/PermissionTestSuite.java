a01d121876218b0987398d2fb61e813db7f39846
Björn Andersson <bjorna@makewave.com>
2022-04-20 17:01:20 +0200

    Updated more copyright years

--- osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/PermissionTestSuite.java
+++ osgi/bundles_test/regression_tests/framework_test/src/org/knopflerfish/bundle/framework_test/PermissionTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, KNOPFLERFISH project
+ * Copyright (c) 2004-2022, KNOPFLERFISH project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
